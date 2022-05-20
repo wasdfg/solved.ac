e469a0e408dfd8c1d1910c7b2c412722737060c3
wasdfg <leejh9711@naver.com>
2022-05-20 20:28:27 +0900

    Add files via upload

---
+++ silverⅡ/1927.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int swap(int *a,int *b){
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+int main(void){
+    int num, cur = 0;
+    scanf("%d", &num);
+    int ins, up;
+    int *array = (int *)malloc(sizeof(int) * (num + 2));
+    memset(array, 0, num + 2);
+    for(int i = 1; i <= num;i++){
+        array[i] = 2147483647;
+    }
+    for (int i = 0; i < num; i++){
+        scanf("%d", &ins);
+        if (ins == 0){
+            if (cur == 0)
+                printf("0\n");
+            else{
+                printf("%d\n", array[1]);
+                array[1] = 2147483647;
+                swap(&array[1],&array[cur]);
+                up = 1;
+                while (1){
+                    if(up * 2 <= cur && array[up * 2] <= array[up * 2 + 1] && array[up] > array[up * 2]){
+						swap(&array[up],&array[up * 2]);
+						up = up * 2;
+					}
+					else if(up * 2 + 1 <= cur && array[up * 2] >= array[up * 2 + 1] && array[up] > array[up * 2 + 1]){
+                        swap(&array[up],&array[up * 2 + 1]);
+						up = up * 2 + 1;
+					}
+					else
+						break;
+                }
+                cur--;      
+            }
+        }
+        else{
+            cur++;
+            array[cur] = ins;
+            up = cur;
+            while (up > 1){
+                if (array[up / 2] > array[up]){
+                    swap(&array[up/2],&array[up]);
+                }
+                up /= 2;
+            }
+        }
+    }
+    free(array);
+}
