d6fe0b3ba0d3fccbba868e33bfd15ac21e447af0
wasdfg <leejh9711@naver.com>
2024-03-24 22:54:09 +0900

    [Silver III] Title: 두 수의 합, Time: 40 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(void){
+    int start = 0,end,n,x,count = 0;
+    cin>>n;
+    end = n-1;
+    vector<int> num(n,0);
+    for(int i = 0;i < n;i++){
+        cin>>num[i];
+    }
+    sort(num.begin(),num.end());
+    cin>>x;
+    while(start < end){
+        if(num[start]+num[end] == x){
+            count++;
+            start++;
+            end--;
+        }
+        if(num[start]+num[end] < x){
+            start++;
+        }
+        else if(num[start]+num[end] > x){
+            end--;
+        }
+    }
+    if(n == 1){
+        cout<<0;
+    }
+    else{
+        cout<<count;
+    }
+    return 0;
+}
