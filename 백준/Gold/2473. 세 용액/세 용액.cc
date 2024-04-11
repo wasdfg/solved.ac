5f83332e915bb8ae9242a2afaf86e83e4ba417ee
wasdfg <leejh9711@naver.com>
2024-04-11 16:14:08 +0900

    [Gold III] Title: 세 용액, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2473. 세 용액/세 용액.cc
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(void){
+    long long n,left,right,a,b,c,check = 3000000001;
+    cin>>n;
+    vector<long long> liq(n,0);
+    for(int i = 0;i < n;i++){
+        cin>>liq[i];
+    }
+    sort(liq.begin(),liq.end());
+    for(int i = 0;i < n-2;i++){
+            left = i+1;
+            right = n-1;
+            while(left < right){
+                if(abs(liq[i]+liq[left]+liq[right]) < check){
+                    a = liq[i];
+                    b = liq[left];
+                    c = liq[right];
+                    check = abs(liq[i]+liq[left]+liq[right]);
+                }
+                if(liq[i]+liq[left]+liq[right] > 0){
+                    right--;
+                }
+                else{
+                    left++;
+                }
+            }
+    }
+    cout<<a<<" "<<b<<" "<<c;
+    return 0;
+}
