990a9d5df445f55d75dbec22636cd6b92fa04477
wasdfg <leejh9711@naver.com>
2024-04-19 16:38:42 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 572 ms, Memory: 2304 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+
+using namespace std;
+
+int main(void){
+    int n,a = 1;
+    int dx[4] = {0,0,-1,1};
+    int dy[4] = {1,-1,0,0};
+    vector<vector<int>> br;
+    vector<vector<int>> dist;
+    while(1){
+        cin>>n;
+        if(n == 0){
+            return 0;
+        }
+        else{
+            br.assign(n,vector<int>(n,0));
+            dist.assign(n,vector<int>(n,987654321));
+            for(int i = 0;i < n;i++){
+                for(int j = 0;j < n;j++){
+                    cin>>br[i][j];
+                }
+            }
+            priority_queue<pair<int,int>> pq;
+            pq.push({0,0});
+            dist[0][0] = br[0][0];
+            while(!pq.empty()){
+                int x = pq.top().first;
+                int y = pq.top().second;
+                pq.pop();
+                for(int i = 0;i < 4;i++){
+                    if(x+dx[i] >= 0 && x+dx[i] < n && y+dy[i] >= 0 && y+dy[i] < n){
+                        if(dist[x+dx[i]][y+dy[i]] > br[x+dx[i]][y+dy[i]]+dist[x][y]){
+                            dist[x+dx[i]][y+dy[i]] = br[x+dx[i]][y+dy[i]]+dist[x][y];
+                            pq.push({x+dx[i],y+dy[i]});
+                        }
+                    }
+                }
+            }
+            cout<<"Problem "<<a<<": "<<dist[n-1][n-1]<<endl;
+        }
+        a++;
+    }
+    return 0;
+}
