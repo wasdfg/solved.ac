3e80d5924b513a914d316d25da0ac9d3d04230ce
wasdfg <leejh9711@naver.com>
2024-05-16 16:59:55 +0900

    [Gold V] Title: 동전 2, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(void){
+    int n,k;
+    cin>>n>>k;
+    vector<int> coin(n,0);
+    vector<int> dp(k+1,100001);
+    for(int i = 0;i < n;i++){
+        cin>>coin[i];
+    }
+    dp[0] = 0;
+    for(int i = 0;i < n;i++){
+        for(int j = coin[i];j <= k;j++){
+            dp[j] = min(dp[j],dp[j-coin[i]]+1);
+        }
+    }
+    if(dp[k] == 100001){
+        cout<<"-1";
+    }
+    else{
+        cout<<dp[k];
+    }
+    return 0;
+}
