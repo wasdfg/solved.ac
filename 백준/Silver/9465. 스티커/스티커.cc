92ca7aa890be1b19bf9f2da2ab2f5b3c2268984c
wasdfg <leejh9711@naver.com>
2024-05-16 15:49:17 +0900

    [Silver I] Title: 스티커, Time: 300 ms, Memory: 3980 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(void){
+    int n,t;
+    cin>>t;
+    for(int a = 0;a < t;a++){
+        cin>>n;
+        vector<vector<int>> sticker(2,vector<int>(n+1,0));
+        vector<vector<int>> dp(2,vector<int>(n+1,0));
+        for(int i = 0;i < 2;i++){
+            for(int j = 0;j < n;j++){
+                cin>>sticker[i][j];
+            }
+        }
+        dp[0][0] = sticker[0][0];
+        dp[1][0] = sticker[1][0];
+        dp[0][1] = sticker[0][1] + dp[1][0];
+        dp[1][1] = sticker[1][1] + dp[0][0];
+        for(int i = 2;i < n;i++){
+            dp[0][i]+= sticker[0][i]+max(dp[1][i-1],dp[1][i-2]);
+            dp[1][i]+= sticker[1][i]+max(dp[0][i-1],dp[0][i-2]);
+        }
+        cout<<max(dp[0][n-1],dp[1][n-1])<<endl;
+    }
+    return 0;
+}
