bd30605211c9b6d1368cbe4d66d8a3c07d00fa9c
wasdfg <leejh9711@naver.com>
2024-06-01 17:12:47 +0900

    [Silver II] Title: 제곱수의 합, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(void){
+    int n;
+    cin>>n;
+    vector<int> dp(n+1,0);
+    for(int i = 1;i < n+1;i++){
+        dp[i] = i;
+    }
+    for(int i = 1;i < n+1;i++){
+        for(int j = 1;j * j <= i;j++){
+            dp[i] = min(dp[i],dp[i-j*j]+1);
+        }
+    }
+    cout<<dp[n];
+    return 0;
+}
