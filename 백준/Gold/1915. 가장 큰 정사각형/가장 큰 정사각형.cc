cb1296e66f812b2954eabb2d29e404abb3877bc1
wasdfg <leejh9711@naver.com>
2024-06-02 16:32:21 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 84 ms, Memory: 5980 KB -BaekjoonHub

---
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(void){
+    int n,m,result = 0;
+    cin>>n>>m;
+    vector<vector<int>> square(n+1,vector<int>(m+1,0));
+    for(int i = 1;i < n+1;i++){
+        for(int j = 1;j < m+1;j++){
+            scanf("%1d",&square[i][j]);
+        }
+    }
+    for(int i = 1;i < n+1;i++){
+        for(int j = 1;j < m+1;j++){
+            if(square[i][j] == 1){
+                square[i][j] = min({square[i-1][j],square[i-1][j-1],square[i][j-1]})+1;
+            }
+        }
+    }
+    for(int i = 1;i < n+1;i++){
+        result = max(result,*max_element(square[i].begin(),square[i].end()));
+    }
+    cout<<result*result;
+    return 0;
+}
