914630a367bd859a862111624be249698ac5c7d3
wasdfg <leejh9711@naver.com>
2024-06-13 16:12:59 +0900

    [Gold V] Title: 1학년, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/5557. 1학년/1학년.cc
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector<vector<long long>> dp;
+
+int main(void){
+    long long n,num;
+    cin>>n;
+    dp.assign(n,vector<long long>(21,0));
+    for(long long i = 0;i < n;i++){
+        cin>>num;
+        if(i == 0){
+            dp[0][num] = 1;
+            continue;
+        }
+        for(long long j = 0;j <= 20;j++){
+            if(dp[i-1][j] == 0){
+                continue;
+            }
+            if(j+num <= 20){
+                dp[i][j+num]+=dp[i-1][j];
+            }
+            if(j-num >= 0){
+                dp[i][j-num]+=dp[i-1][j];
+            }
+        }
+        if(i == n-1){
+            cout<<dp[i-1][num];
+        }
+    }
+    return 0;
+}
