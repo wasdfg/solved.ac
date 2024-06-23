d8a273a3484ac0362418789ef803201c80b0f634
wasdfg <leejh9711@naver.com>
2024-06-23 14:16:14 +0900

    [Gold V] Title: 공약수, Time: 872 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2436. 공약수/공약수.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+#define ull unsigned long long
+
+using namespace std;
+
+ull gcd(ull x,ull y){
+    if(x < y){
+        swap(x,y);
+    }
+    if(y == 0){
+        return x;
+    }
+    else{
+        return gcd(y,x%y);
+    }
+}
+
+int main(void){
+    ull a,b,c = 2,d = 1000000001;
+    cin>>a>>b;
+    for(int i = 1;i <= b;i++){
+        if((a*b) % (a*i) == 0 && gcd((a*b)/(a*i),(a*i)) == a){
+            if(d-c > ((a*b)/(a*i)-(a*i))){
+                d = (a*b)/(a*i);
+                c = a*i;
+            }
+        }
+    }
+    cout<<c<<" "<<d;
+    return 0;
+}
