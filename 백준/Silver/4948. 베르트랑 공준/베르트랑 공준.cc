d0ce3c5b8d728cb86c4dff76c1edb9c8920b3f92
wasdfg <leejh9711@naver.com>
2024-06-24 16:08:21 +0900

    [Silver II] Title: 베르트랑 공준, Time: 24 ms, Memory: 3000 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(void){
+    int t,n;
+    vector<int> prime(250000,1);
+    prime[1] = 0;
+    for(int i = 2;i*i <= 250000;i++){
+        for(int j = 2;j*i <= 250000;j++){
+            prime[i*j] = 0;
+        }
+    }
+    while(cin>>n){
+        if(n == 0){
+            return 0;
+        }
+        int count = 0;
+        for(int j = n+1;j <= 2*n;j++){
+            if(prime[j] == 1){
+                count++;
+            }
+        }
+        cout<<count<<endl;
+    }
+    return 0;
+}
