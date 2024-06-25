7d0263ebeaa30053851461e7aa0b2fd5934fbc1f
wasdfg <leejh9711@naver.com>
2024-06-25 16:19:30 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 416 ms, Memory: 6044 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int w[101][101][101];
+
+int main(void){
+    int a,b,c;
+    fill_n(w[0][0],101*101*101,1);
+    for(int i = 51;i < 71;i++){
+        for(int j = 51;j < 71;j++){
+            for(int k = 51;k < 71;k++){
+                if(i < j && j < k){
+                    w[i][j][k] = w[i][j][k-1]+w[i][j-1][k-1]-w[i][j-1][k];
+                }
+                else{
+                    w[i][j][k] = w[i-1][j][k]+w[i-1][j-1][k]+w[i-1][j][k-1]-w[i-1][j-1][k-1];
+                }
+            }
+        }
+    }
+    for(int i = 51;i < 101;i++){
+        for(int j = 51;j < 101;j++){
+            for(int k = 51;k < 101;k++){
+                if(i >= 71 || j >= 71 || k >= 71){
+                    w[i][j][k] = w[70][70][70];
+                }
+            }
+        }
+    }
+    while(1){
+        cin>>a>>b>>c;
+        if(a == -1 && b == -1 && c == -1){
+            return 0;
+        }
+        else{
+            cout<<"w("<<a<<", "<<b<<", "<<c<<") = "<<w[a+50][b+50][c+50]<<endl;
+        }
+    }
+    return 0;
+}
