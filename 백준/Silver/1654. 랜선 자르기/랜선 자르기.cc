88349ee08760b35185fd53a55ccfa8e5c3d2fb7c
wasdfg <leejh9711@naver.com>
2024-06-26 15:06:53 +0900

    [Silver II] Title: 랜선 자르기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(void){
+    long long n,k,left = 1,right,count,result = 0;
+    cin>>k>>n;
+    vector<long long> len(k,0);
+    for(int i = 0;i < k;i++){
+        cin>>len[i];
+    }
+    sort(len.begin(),len.end());
+    right = len[k-1];
+    while(left <= right){
+        long long mid = (left+right)/2;
+        count = 0;
+        for(int i = 0;i < k;i++){
+            count+=(len[i]/mid);
+        }
+        if(count >= n){
+            result = max(result,mid);
+            left = mid+1;
+        }
+        else{
+            right = mid-1;
+        }
+    }
+    cout<<result;
+    return 0;
+}
