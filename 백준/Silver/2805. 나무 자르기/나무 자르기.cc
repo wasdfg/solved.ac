7665832f664ced2e46ec7dd54e8ff7760c211a76
wasdfg <leejh9711@naver.com>
2024-06-26 15:38:04 +0900

    [Silver II] Title: 나무 자르기, Time: 684 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(void){
+    long long n,m,left = 1,right,count,result = 0;
+    cin>>n>>m;
+    vector<long long> tree(n,0);
+    for(int i = 0;i < n;i++){
+        cin>>tree[i];
+    }
+    sort(tree.begin(),tree.end());
+    right = tree[n-1];
+    while(left <= right){
+        long long mid = (left+right)/2;
+        count = 0;
+        for(int i = 0;i < n;i++){
+            count+=(tree[i] <= mid?0:tree[i]-mid);
+        }
+        if(count >= m){
+            result = max(result,mid);
+            left = mid+1;
+        }
+        else{
+            right = mid-1;
+        }
+    }
+    cout<<result;
+    return 0;
+}
