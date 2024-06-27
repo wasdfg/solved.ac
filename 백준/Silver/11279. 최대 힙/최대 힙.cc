6350f5894ec0e1a7cf76e97b8c280fd17fefb30d
wasdfg <leejh9711@naver.com>
2024-06-27 15:27:16 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,x;
+    priority_queue<int> pq;
+    cin>>n;
+    for(int i = 0;i < n;i++){
+        cin>>x;
+        if(x == 0){
+            if(pq.empty()){
+                cout<<"0\n";
+            }
+            else{
+                cout<<pq.top()<<"\n";
+                pq.pop();
+            }
+        }
+        else{
+            pq.push(x);
+        }
+    }
+    return 0;
+}
