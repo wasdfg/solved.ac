985927238b8fa0a52ab198d4eba3393a38c305fd
wasdfg <leejh9711@naver.com>
2024-06-29 16:00:22 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 232 ms, Memory: 9448 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+vector<int> graph[100001];
+
+int main(void){
+    int n,m,r,a,b,cnt = 1;
+    vector<int> before(100001,0);
+    vector<bool> visited(100001,0);
+    queue<int> togo;
+    cin>>n>>m>>r;
+    for(int i = 0;i < m;i++){
+        cin>>a>>b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+    for(int i = 1;i <= n;i++){
+        sort(graph[i].begin(),graph[i].end());
+    }
+    visited[r] = 1;
+    before[r] = cnt;
+    togo.push(r);
+    while(!togo.empty()){
+        int x = togo.front();
+        togo.pop();
+        for(int i = 0;i < graph[x].size();i++){
+            if(visited[graph[x][i]] == 0){
+                visited[graph[x][i]] = 1;
+                cnt++;
+                before[graph[x][i]] = cnt;
+                togo.push({graph[x][i]});
+            }
+        }
+    }
+    for(int i = 1;i <= n;i++){
+        cout<<before[i]<<"\n";
+    }
+    return 0;
+}
