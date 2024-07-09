896afa323a4c2443ae3662c5ee12d5284afa5719
wasdfg <leejh9711@naver.com>
2024-07-09 14:43:11 +0900

    [Silver III] Title: 수열, Time: 28 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -4,24 +4,21 @@
 using namespace std;
 
 int main(void){
-    int max = -10000001,N,K,x,sum = 0;
-    vector<int> num;
-    cin>>N>>K;
-    for(int i = 0;i < N;i++){
-        cin>>x;
-        num.push_back(x);
+    int n,k,result = -10000001,check = 0;
+    cin>>n>>k;
+    vector<int> degree(n,0);
+    for(int i = 0;i < n;i++){
+        cin>>degree[i];
     }
-    for(int i = 0;i < K;i++){
-        sum+=num[i];
+    for(int i = 0;i < k;i++){
+        check+=degree[i];
     }
-    if(sum > max)
-        max = sum;
-    for(int i = 0;i < N - K;i++){
-        sum -= num[i];
-        sum += num[i+K];
-        if(sum > max){
-            max = sum;
-        }
+    result = max(check,result);
+    for(int i = 0;i < n-k;i++){
+        check-=degree[i];
+        check+=degree[k+i];
+        result = max(check,result);
     }
-    cout<<max<<endl;
+    cout<<result;
+    return 0;
 }
