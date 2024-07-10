ca2de79235a4cc4fe38f85f4e6ac193dd521c918
wasdfg <leejh9711@naver.com>
2024-07-10 14:33:57 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -5,23 +5,23 @@
 
 using namespace std;
 
-vector<int> vertex[1002];
-vector<bool> visited(1002,false);
-vector<int> bfs[1002];
-vector<int> dfs[1002];
+vector<int> vertex[1001];
+vector<bool> visited(1001,0);
+vector<int> dfs;
+vector<int> bfs;
 
 int BFS(int start){
     queue<int> togo;
     togo.push(start);
-    visited[start] = true;
+    visited[start] = 1;
     while(!togo.empty()){
         int x = togo.front();
         cout<<x<<" ";
         togo.pop();
-        for(int i = 0;i < bfs[x].size();i++){
-            if(!visited[bfs[x][i]]){
-                togo.push(bfs[x][i]);
-                visited[bfs[x][i]] = true;
+        for(int i = 0;i < vertex[x].size();i++){
+            if(!visited[vertex[x][i]]){
+                togo.push(vertex[x][i]);
+                visited[vertex[x][i]] = 1;
             }
         }
     }
@@ -29,11 +29,11 @@ int BFS(int start){
 }
 
 int DFS(int start){
-    visited[start] = true;
+    visited[start] = 1;
     cout<<start<<" ";
-    for(int i = 0;i < dfs[start].size();i++){
-        if(!visited[dfs[start][i]]){
-            DFS(dfs[start][i]);
+    for(int i = 0;i < vertex[start].size();i++){
+        if(!visited[vertex[start][i]]){
+            DFS(vertex[start][i]);
         }
     }
     return 0;
@@ -41,23 +41,18 @@ int DFS(int start){
 
 int main(void){
     int dot,line,start,a,b;
-    scanf("%d %d %d",&dot,&line,&start);
+    cin>>dot>>line>>start;
     for(int i = 0;i < line;i++){
-        scanf("%d %d",&a,&b);
-        bfs[a].push_back(b);
-        bfs[b].push_back(a);
-        dfs[a].push_back(b);
-        dfs[b].push_back(a);
+        cin>>a>>b;
+        vertex[a].push_back(b);
+        vertex[b].push_back(a);
     }
-    for(int i = 1;i < 1002;i++){
-        stable_sort(bfs[i].begin(),bfs[i].end());
-        stable_sort(dfs[i].begin(),dfs[i].end());
+    for(int i = 1;i < 1001;i++){
+        sort(vertex[i].begin(),vertex[i].end());
     }
     DFS(start);
     cout<<"\n";
-    for(int i = 0;i < 1002;i++){
-        visited[i] = false;
-    }
+    visited.assign(1001,0);
     BFS(start);
     cout<<"\n";
     return 0;
