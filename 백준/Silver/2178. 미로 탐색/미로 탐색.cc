70000c9f2a4b89ef02b2f4441d18329cdb9824bd
wasdfg <leejh9711@naver.com>
2024-07-11 14:44:08 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -5,38 +5,36 @@
 using namespace std;
 
 int main(void){
-    int N,M;
-    cin>>N>>M;
-    vector<vector<int>> maze(N,vector<int>(M,0));
-    vector<vector<int>> dp(N,vector<int>(M,0));
+    int n,m;
+    cin>>n>>m;
+    int dx[4] = {0,0,-1,1};
+    int dy[4] = {1,-1,0,0};
+    vector<vector<int>> maze(n,vector<int>(m,0));
+    vector<vector<int>> dp(n,vector<int>(m,0));
     queue<pair<int,int>> togo;
-    int tx[4] = {0,0,-1,1};
-    int ty[4] = {1,-1,0,0};
-    for(int i = 0;i < N;i++){
-        for(int j = 0;j < M;j++){
+    for(int i = 0;i < n;i++){
+        for(int j = 0;j < m;j++){
             scanf("%1d",&maze[i][j]);
         }
     }
+    dp[0][0] = 1;
     maze[0][0] = 0;
     togo.push({0,0});
-    dp[0][0]++;
     while(!togo.empty()){
         int x = togo.front().first;
         int y = togo.front().second;
         togo.pop();
-        if(x == N -1 && y == M -1){
+        if(x == n-1 && y == m-1){
             break;
         }
         for(int i = 0;i < 4;i++){
-            if(x + tx[i] >= 0 && x + tx[i] < N && y + ty[i] >= 0 && y+ty[i] < M){
-                if(maze[x + tx[i]][y + ty[i]] == 1){
-                    maze[x + tx[i]][y + ty[i]] = 0;
-                    togo.push({x + tx[i],y + ty[i]});
-                    dp[x + tx[i]][y + ty[i]] = dp[x][y] + 1;
-                }
+            if(x+dx[i] >= 0 && x+dx[i] < n && y+dy[i] >= 0 && y+dy[i] < m && maze[x+dx[i]][y+dy[i]] == 1){
+                dp[x+dx[i]][y+dy[i]] = dp[x][y]+1;
+                maze[x+dx[i]][y+dy[i]] = 0;
+                togo.push({x+dx[i],y+dy[i]});
             }
         }
     }
-    cout<<dp[N-1][M-1]<<endl;
+    cout<<dp[n-1][m-1];
     return 0;
 }
