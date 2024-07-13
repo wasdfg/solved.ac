7e72bde4f90e2ab0b5b29fbdcf2524b773d472c4
wasdfg <leejh9711@naver.com>
2024-07-13 15:12:40 +0900

    [Gold V] Title: 토마토, Time: 224 ms, Memory: 7384 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.cc
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -7,54 +7,53 @@ using namespace std;
 int tomato[100][100][100];
 
 int main(void){
-    int M,N,H,days = 0;
-    int tx[6] = {0,0,-1,1,0,0};
-    int ty[6] = {1,-1,0,0,0,0};
-    int tz[6] = {0,0,0,0,1,-1};
-    cin>>M>>N>>H;
-
+    int n,m,h,result = 0;
+    int dx[6] = {1,-1,0,0,0,0};
+    int dy[6] = {0,0,-1,1,0,0};
+    int dz[6] = {0,0,0,0,-1,1};
     queue<tuple<int,int,int>> togo;
-    for(int i = 0;i < H;i++){
-        for(int j = 0;j < N;j++){
-            for(int k = 0;k < M;k++){
-                scanf("%d",&tomato[i][j][k]);
-            }
-        }
-    }
-    for(int i = 0;i < H;i++){
-        for(int j = 0;j < N;j++){
-            for(int k = 0;k < M;k++){
+    cin>>m>>n>>h;
+    for(int i = 0;i < h;i++){
+        for(int j = 0;j < n;j++){
+            for(int k = 0;k < m;k++){
+                cin>>tomato[i][j][k];
                 if(tomato[i][j][k] == 1){
-                    togo.push(make_tuple(i,j,k));
+                    togo.push({i,j,k});
                 }
             }
         }
     }
     while(!togo.empty()){
-        int x = get<1>(togo.front());
-        int y = get<2>(togo.front());
-        int z = get<0>(togo.front());
+        int x = get<0>(togo.front());
+        int y = get<1>(togo.front());
+        int z = get<2>(togo.front());
         togo.pop();
         for(int i = 0;i < 6;i++){
-            if(x+tx[i] >= 0 && x+tx[i] < N && y+ty[i] >= 0 && y+ty[i] < M && z+tz[i] >= 0 && z+tz[i] < H && tomato[z+tz[i]][x+tx[i]][y+ty[i]] == 0){
-                tomato[z+tz[i]][x+tx[i]][y+ty[i]] = tomato[z][x][y] + 1;
-                togo.push({z+tz[i],x+tx[i],y+ty[i]});
+            if(x+dx[i] >= 0 && x+dx[i] < h && y+dy[i] >= 0 && y+dy[i] < n && z+dz[i] >= 0 && z+dz[i] < m){
+                if(tomato[x+dx[i]][y+dy[i]][z+dz[i]] == 0){
+                    tomato[x+dx[i]][y+dy[i]][z+dz[i]] = tomato[x][y][z]+1;
+                    togo.push({x+dx[i],y+dy[i],z+dz[i]});
+                }
+            } 
+        }
+    }
+    bool cant = false;
+    for(int i = 0;i < h;i++){
+        for(int j = 0;j < n;j++){
+            for(int k = 0;k < m;k++){
+                if(tomato[i][j][k] == 0){
+                    cant = true;
+                    break;
+                }
+                result = max(result,tomato[i][j][k]);
             }
         }
     }
-    for(int i = 0;i < H;i++){
-        for(int j = 0;j < N;j++){
-            for(int k = 0;k < M;k++){
-            if (tomato[i][j][k] == 0){
-                cout << -1 << endl;
-                return 0;
-            }
-            if (days < tomato[i][j][k]){ 
-                days = tomato[i][j][k];
-            }
-        }
+    if(cant == 1){
+        cout<<"-1";
     }
+    else{
+        cout<<result-1;
     }
-    cout <<days-1;
     return 0;
 }
