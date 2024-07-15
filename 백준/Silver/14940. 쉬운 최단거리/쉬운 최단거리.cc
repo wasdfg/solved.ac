0f46d915c708ddcae39620e844cd3aa826a94264
wasdfg <leejh9711@naver.com>
2024-07-15 14:25:18 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 80 ms, Memory: 3080 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.cc
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.cc
@@ -4,31 +4,21 @@
 
 using namespace std;
 
-vector<vector<int>> bfs;
-vector<vector<int>> dp;
-queue<pair<int,int>> togo;
-int tx[4] = {0,0,-1,1};
-int ty[4] = {1,-1,0,0};
-
 int main(void){
     int n,m;
     cin>>n>>m;
-    bfs.resize(n,vector<int>(m,0));
-    dp.resize(n,vector<int>(m,-1));
+    vector<vector<int>> zido(n,vector<int>(m,0));
+    vector<vector<bool>> visited(n,vector<bool>(m,0));
+    queue<pair<int,int>> togo;
+    int dx[4] = {0,0,-1,1};
+    int dy[4] = {1,-1,0,0};
     for(int i = 0;i < n;i++){
         for(int j = 0;j < m;j++){
-            cin>>bfs[i][j];
-            if(bfs[i][j] == 0){
-                dp[i][j] = 0;
-            }
-        }
-    }
-    for(int i = 0;i < n;i++){
-        for(int j = 0;j < m;j++){
-            if(bfs[i][j] == 2){
+            cin>>zido[i][j];
+            if(zido[i][j] == 2){
                 togo.push({i,j});
-                dp[i][j] = 0;
-                break;
+                visited[i][j] = 1;
+                zido[i][j] = 1;
             }
         }
     }
@@ -37,19 +27,26 @@ int main(void){
         int y = togo.front().second;
         togo.pop();
         for(int i = 0;i < 4;i++){
-            if(x+tx[i] >= 0 && x+tx[i] < n && y+ty[i] >= 0 && y+ty[i] < m){
-                if(bfs[x+tx[i]][y+ty[i]] == 1){
-                    bfs[x+tx[i]][y+ty[i]] = 0;
-                    dp[x+tx[i]][y+ty[i]] = dp[x][y]+1;
-                    togo.push({x+tx[i],y+ty[i]});
-                }
+            if(x+dx[i] >= 0 && x+dx[i] < n && y+dy[i] >= 0 && y+dy[i] < m && visited[x+dx[i]][y+dy[i]] == 0 && zido[x+dx[i]][y+dy[i]] == 1){
+                visited[x+dx[i]][y+dy[i]] = 1;
+                zido[x+dx[i]][y+dy[i]] = zido[x][y]+1;
+                togo.push({x+dx[i],y+dy[i]});
             }
         }
     }
     for(int i = 0;i < n;i++){
         for(int j = 0;j < m;j++){
-            cout<<dp[i][j]<<" ";
+            if(zido[i][j] != 0 && visited[i][j] == 1){
+                cout<<zido[i][j]-1<<" ";
+            }
+            else if(zido[i][j] == 0){
+                cout<<"0 ";
+            }
+            else if(zido[i][j] == 1 && visited[i][j] == 0){
+                cout<<"-1 ";
+            }
         }
-        cout<<"\n";
+        cout<<endl;
     }
+    return 0;
 }
