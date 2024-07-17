c881604f1cb3ecdde2d3d9df03c3ee96bcb2e3fa
wasdfg <leejh9711@naver.com>
2024-07-17 15:07:17 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -6,12 +6,12 @@ using namespace std;
 int main(void){
     int n;
     cin>>n;
-    vector<unsigned long long> n2(n,0);
-    n2[0] = 1;
-    n2[1] = 2;
+    vector<long long> dp(n,0);
+    dp[0] = 1;
+    dp[1] = 2;
     for(int i = 2;i < n;i++){
-        n2[i] = ((n2[i-1] % 10007) + (n2[i-2] % 10007)) % 10007;
+        dp[i] = ((dp[i-1] % 10007) + (dp[i-2] % 10007)) % 10007;
     }
-    cout<<n2[n-1];
+    cout<<dp[n-1];
     return 0;
 }
