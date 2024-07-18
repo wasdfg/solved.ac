cf2c8846825841a7df418463337ce5de1e904540
wasdfg <leejh9711@naver.com>
2024-07-18 16:25:01 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2416 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -4,44 +4,37 @@
 
 using namespace std;
 
-vector<int> on(100001,0);
-vector<bool> visited(100001,0);
-
 int main(void){
-    int N,K;
-    cin>>N>>K;
-    queue<int> togo;
-    visited[N] = true;
-    on[N] = 1;
-    togo.push(N);
-    while(!togo.empty()){
-        int x = togo.front();
-        togo.pop();
-        if(x == K){
+    int n,k;
+    cin>>n>>k;
+    vector<int> pos(100001,100000000);
+    vector<bool> visited(100001,0);
+    queue<int> now;
+    now.push(n);
+    visited[n] = 1;
+    pos[n] = 0;
+    while(!now.empty()){
+        int here = now.front();
+        now.pop();
+        if(here == k){
             break;
         }
-        if(x - 1 >= 0){
-            if(visited[x-1] == false){
-                visited[x-1] = true;
-                on[x-1] = on[x] + 1;
-                togo.push(x-1);
-            }
+        if(here+1 < 100001 && visited[here+1] == 0){
+            pos[here+1] = min(pos[here+1],pos[here]+1);
+            visited[here+1] = 1;
+            now.push(here+1);
         }
-        if(x + 1 <= 100000){
-            if(visited[x+1] == false){
-                visited[x+1] = true;
-                on[x+1] = on[x] +1;
-                togo.push(x+1);
-            }
+        if(here-1 >= 0 && visited[here-1] == 0){
+            pos[here-1] = min(pos[here-1],pos[here]+1);
+            visited[here-1] = 1;
+            now.push(here-1);
         }
-        if(x * 2 <= 100000){
-            if(visited[x*2] == false){
-                visited[x*2] = true;
-                on[x*2] = on[x] + 1;
-                togo.push(x*2);
-            }
+        if(here*2 < 100001 && visited[here*2] == 0){
+            pos[here*2] = min(pos[here*2],pos[here]+1);
+            visited[here*2] = 1;
+            now.push(here*2);
         }
     }
-    cout<<on[K]-1;
+    cout<<pos[k];
     return 0;
 }
