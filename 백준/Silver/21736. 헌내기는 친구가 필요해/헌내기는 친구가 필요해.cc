4e6a77d92dccf090f2f32eeca864b4f9d0713004
wasdfg <leejh9711@naver.com>
2024-07-19 14:24:31 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 36 ms, Memory: 3748 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.cc
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.cc
@@ -4,18 +4,17 @@
 
 using namespace std;
 
-vector<vector<int>> campus;
-
 int main(void){
-    int N,M,count = 0;
-    int tx[4] = {0,0,-1,1};
-    int ty[4] = {1,-1,0,0};
+    int n,m,count = 0;
+    int dx[4] = {0,0,-1,1};
+    int dy[4] = {1,-1,0,0};
     char point;
-    cin>>N>>M;
-    campus.resize(N,vector<int>(M,0));
+    cin>>n>>m;
+    vector<vector<int>> campus(n,vector<int>(m,0));
+    vector<vector<bool>> visited(n,vector<bool>(m,0));
     queue<pair<int,int>> togo;
-    for(int i = 0;i < N;i++){
-        for(int j = 0;j < M;j++){
+    for(int i = 0;i < n;i++){
+        for(int j = 0;j < m;j++){
             cin>>point;
             if(point == 'O'){
                 campus[i][j] = 0;
@@ -24,11 +23,12 @@ int main(void){
                 campus[i][j] = -1;
             }
             if(point == 'I'){
-                campus[i][j] = 1;
+                campus[i][j] = 0;
                 togo.push({i,j});
+                visited[i][j] = 1; 
             }
             if(point == 'P'){
-                campus[i][j] = 2;
+                campus[i][j] = 1;
             }
         }
     }
@@ -37,16 +37,12 @@ int main(void){
         int y = togo.front().second;
         togo.pop();
         for(int i = 0;i < 4;i++){
-            if(x+tx[i] >= 0 && x+tx[i] < N && y+ty[i] >= 0 && y+ty[i] < M){
-                if(campus[x+tx[i]][y+ty[i]] == 0){
-                    campus[x+tx[i]][y+ty[i]] = 1;
-                    togo.push({x+tx[i],y+ty[i]});
-                }
-                if(campus[x+tx[i]][y+ty[i]] == 2){
-                    campus[x+tx[i]][y+ty[i]] = 1;
+            if(x+dx[i] >= 0 && x+dx[i] < n && y+dy[i] >= 0 && y+dy[i] < m && campus[x+dx[i]][y+dy[i]] >= 0 && visited[x+dx[i]][y+dy[i]] == 0){
+                if(campus[x+dx[i]][y+dy[i]] == 1){
                     count++;
-                    togo.push({x+tx[i],y+ty[i]});
                 }
+                visited[x+dx[i]][y+dy[i]] = 1;
+                togo.push({x+dx[i],y+dy[i]});
             }
         }
     }
