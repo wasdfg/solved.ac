d035b819b34868032d941ae80e3a555ce5ebdd08
wasdfg <leejh9711@naver.com>
2024-07-21 15:38:25 +0900

    [Gold IV] Title: 최단경로, Time: 116 ms, Memory: 9064 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -4,59 +4,44 @@
 
 using namespace std;
 
-vector<pair<int,int>> graph[20001];
-vector<int> mincost;
-int V,E,K;
-
-int daijkstra(int start){
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    bool visited[20001] = {0,};
-    pq.push({0,start});
-    mincost[start] = 0;
-    while(!pq.empty()){
-        int cost = pq.top().first;
-        int cur = pq.top().second;
-        pq.pop();
-        if(visited[cur] == true){
-            continue;
-        }
-        visited[cur] = true;
-        for(int i = 0;i < graph[cur].size();i++){
-            int ncost = graph[cur][i].second;
-            int next = graph[cur][i].first;
-            if(mincost[next] > ncost + cost){
-                mincost[next] = cost+ncost;
-                pq.push({mincost[next],next});
-            }
-        }
-    }
-    return 0;
-}
-
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int u,v,e;
-    cin>>V>>E;
-    cin>>K;
-    mincost.resize(V+1,99999999);
-    for(int i = 0;i < E;i++){
-        cin>>u>>v>>e;
-        graph[u].push_back({v,e});
+    int v,e,k,a,b,c;
+    cin>>v>>e>>k;
+    vector<pair<int,int>> graph[v+1];
+    vector<int> take(v+1,2000000000);
+    priority_queue<pair<int,int>> togo;
+    for(int i = 0;i < e;i++){
+        cin>>a>>b>>c;
+        graph[a].push_back({b,c});
     }
-    daijkstra(K);
-    for(int i = 1;i <= V;i++){
-        if(i == K){
-            cout<<"0\n";
+    take[k] = 0;
+    togo.push({0,k});
+    while(!togo.empty()){
+        int now = togo.top().second;
+        int cost = -togo.top().first;
+        togo.pop();
+        if(cost > take[now]){
+            continue;
+        }
+        for(int i = 0;i < graph[now].size();i++){
+            int next = graph[now][i].first;
+            int ncost = cost+graph[now][i].second;
+            if(ncost < take[next]){
+                take[next] = ncost;
+                togo.push({-ncost,next});
+            }
+        }
+    }
+    for(int i = 1;i < v+1;i++){
+        if(take[i] == 2000000000){
+            cout<<"INF\n";
         }
         else{
-            if(mincost[i] == 99999999){
-                cout<<"INF\n";
-            }
-            else{
-                cout<<mincost[i]<<endl;
-            }
+            cout<<take[i]<<"\n";
         }
     }
+    return 0;
 }
