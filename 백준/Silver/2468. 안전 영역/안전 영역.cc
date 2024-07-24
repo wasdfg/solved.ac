cf464e792745e2c8bee4195c47cd57cdeb7b1973
wasdfg <leejh9711@naver.com>
2024-07-24 13:55:10 +0900

    [Silver I] Title: 안전 영역, Time: 24 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -3,40 +3,38 @@
 #include<queue>
 
 using namespace std;
-vector<vector<int>> rain;
-vector<vector<bool>> visited;
 
 int main(void){
-    int N,maxv = 0,area = 0,maxarea = 0;
-    cin>>N;
-    rain.resize(N,vector<int>(N,0));
-    visited.resize(N,vector<bool>(N,0));
+    int n,height = 0,area = 0,maxarea = 1;
+    cin>>n;
+    vector<vector<int>> rain(n,vector<int>(n,0));
+    vector<vector<bool>> visited(n,vector<bool>(n,0));
     queue<pair<int,int>> togo;
-    int tx[4] = {0,0,-1,1};
-    int ty[4] = {1,-1,0,0};
-    for(int i = 0;i < N;i++){
-        for(int j = 0;j < N;j++){
+    int dx[4] = {0,0,-1,1};
+    int dy[4] = {1,-1,0,0};
+    for(int i = 0;i < n;i++){
+        for(int j = 0;j < n;j++){
             cin>>rain[i][j];
-            if(maxv < rain[i][j]){
-                maxv = rain[i][j];
+            if(height < rain[i][j]){
+                height = rain[i][j];
             }
         }
     }
-    for(int i = 0;i <= maxv;i++){
-        for(int j = 0;j < N;j++){
-            for(int k = 0;k < N;k++){
-                if(rain[j][k] > i && visited[j][k] == false){
-                    togo.push({j,k});
-                    visited[j][k] == true;
+    for(int k = 1;k <= height;k++){
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < n;j++){
+                if(rain[i][j] > k && visited[i][j] == 0){
+                    togo.push({i,j});
+                    visited[i][j] = 1;
                     while(!togo.empty()){
                         int x = togo.front().first;
                         int y = togo.front().second;
                         togo.pop();
                         for(int l = 0;l < 4;l++){
-                            if(x+tx[l] >= 0 && x+tx[l] < N && y+ty[l] >= 0 && y+ty[l] < N){
-                                if(rain[x+tx[l]][y+ty[l]] > i && visited[x+tx[l]][y+ty[l]] == false){
-                                    visited[x+tx[l]][y+ty[l]] = true;
-                                    togo.push({x+tx[l],y+ty[l]});
+                            if(x+dx[l] >= 0 && x+dx[l] < n && y+dy[l] >= 0 && y+dy[l] < n){
+                                if(rain[x+dx[l]][y+dy[l]] > k && visited[x+dx[l]][y+dy[l]] == 0){
+                                    visited[x+dx[l]][y+dy[l]] = 1;
+                                    togo.push({x+dx[l],y+dy[l]});
                                 }
                             }
                         }
@@ -49,7 +47,7 @@ int main(void){
             maxarea = area;
         }
         area = 0;
-        visited.assign(N,vector<bool>(N,false));
+        visited.assign(n,vector<bool>(n,0));
     }
     cout<<maxarea;
     return 0;
