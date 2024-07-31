da9b6997a317ea1e0c5323cec0585c4859276048
wasdfg <leejh9711@naver.com>
2024-07-31 14:54:34 +0900

    [Silver I] Title: 신입 사원, Time: 1652 ms, Memory: 4080 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.cc
+++ 백준/Silver/1946. 신입 사원/신입 사원.cc
@@ -5,26 +5,25 @@
 using namespace std;
 
 int main(void){
-    int T,N,a,b,peer;
-    vector<pair<int,int>> order;
-    cin>>T;
-    for(int i = 0;i < T;i++){
-        order.clear();
-        cin>>N;
-        peer = 0;
-        for(int j = 0;j < N;j++){
+    int t,n,a,b,result;
+    cin>>t;
+    for(int i = 0;i < t;i++){
+        result = 0;
+        cin>>n;
+        vector<pair<int,int>> recruit;
+        for(int j = 0;j < n;j++){
             cin>>a>>b;
-            order.push_back({a,b});
+            recruit.push_back({a,b});
         }
-        sort(order.begin(),order.end());
-        int mini = 1000001;
-        for(int j = 0;j < N;j++){
-            if(mini > order[j].second){
-                peer++;
-                mini = order[j].second;
+        sort(recruit.begin(),recruit.end());
+        int least = 100001;
+        for(int j = 0;j < n;j++){
+            if(least > recruit[j].second){
+                result++;
+                least = recruit[j].second;
             }
         }
-        cout<<peer<<endl;
+        cout<<result<<endl;
     }
     return 0;
 }
