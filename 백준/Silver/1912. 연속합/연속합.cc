d2f0585887b59878d98dc947b9c5df051a643fa6
wasdfg <leejh9711@naver.com>
2024-08-04 16:08:03 +0900

    [Silver II] Title: 연속합, Time: 20 ms, Memory: 3588 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -4,19 +4,19 @@
 using namespace std;
 
 int main(void){
-    long long n,number,maxi;
+    long long n,result = 0;
     cin>>n;
-    vector<long long> sum;
-    vector<long long> result(n,0);
+    vector<long long> num(n,0);
+    vector<long long> dp(n,0);
     for(int i = 0;i < n;i++){
-        cin>>number;
-        sum.push_back(number);
+        cin>>num[i];
     }
-    result[0] = sum[0];
-    maxi = result[0];
+    dp[0] = num[0];
+    result = dp[0];
     for(int i = 1;i < n;i++){
-        maxi = max(maxi,result[i] = max(sum[i]+result[i-1],sum[i])); 
+        dp[i] = max(dp[i-1]+num[i],num[i]);
+        result = max(dp[i],result);
     }
-    cout<<maxi;
+    cout<<result;
     return 0;
 }
