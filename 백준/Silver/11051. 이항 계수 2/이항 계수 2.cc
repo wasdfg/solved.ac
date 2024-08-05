426a3f7a0dc6a6da33e87b078c87a67d779534ef
wasdfg <leejh9711@naver.com>
2024-08-05 14:39:26 +0900

    [Silver II] Title: 이항 계수 2, Time: 4 ms, Memory: 9876 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
@@ -1,23 +1,18 @@
 #include<iostream>
 #include<vector>
 typedef unsigned long long ull;
-using namespace std;
 
+using namespace std;
 
 int main(void){
     int n,k;
     cin>>n>>k;
-    vector<vector<ull>> c(n+1,vector<ull>(n+1,0));
-    for(int i = 0;i <= n;i++){
-        for(int j = 0;j <= i;j++){
-            if(i == j || j == 0){
-                c[i][j] = 1;
-            }
-            else{
-                c[i][j] = ((c[i-1][j] % 10007) + (c[i-1][j-1] % 10007)) % 10007;
-            }
+    vector<vector<ull>> dp(n+1,vector<ull>(n+1,1));
+    for(int i = 1;i < n+1;i++){
+        for(int j = 1;j < i;j++){
+            dp[i][j] = (dp[i-1][j] % 10007)+(dp[i-1][j-1] % 10007);
         }
     }
-    cout<<c[n][k];
+    cout<<dp[n][k] % 10007;
     return 0;
 }
