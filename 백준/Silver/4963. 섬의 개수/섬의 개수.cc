6fbc5aaacea0c1ebd9bac80d3592f9a01cd7f7fa
wasdfg <leejh9711@naver.com>
2024-08-06 12:58:21 +0900

    [Silver II] Title: 섬의 개수, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.cc
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.cc
@@ -1,23 +1,29 @@
 #include<iostream>
 #include<vector>
-#include<queue>
 
 using namespace std;
+
 vector<vector<bool>> island;
 vector<vector<bool>> visited;
-queue<pair<int,int>> togo;
-int ty[8] = {0,0,-1,1,-1,1,-1,1};
-int tx[8] = {1,-1,0,0,-1,1,1,-1};
+int dy[8] = {0,0,-1,1,-1,1,-1,1};
+int dx[8] = {1,-1,0,0,-1,1,1,-1};
+int w,h,count;
+
+void dfs(int x,int y){
+    for(int i = 0;i < 8;i++){
+        if(x+dx[i] >= 0 && x+dx[i] < h && y+dy[i] >= 0 && y+dy[i] < w && island[x+dx[i]][y+dy[i]] == 1 && visited[x+dx[i]][y+dy[i]] == 0){
+            visited[x+dx[i]][y+dy[i]] = 1;
+            dfs(x+dx[i],y+dy[i]);
+        }
+    }
+}
 
 int main(void){
-    int w,h,count;
     while(1){
         cin>>w>>h;
         if(w == 0 && h == 0){
             break;
         }
-        island.resize(h,vector<bool>(w,0));
-        visited.resize(h,vector<bool>(w,0));
         island.assign(h,vector<bool>(w,0));
         visited.assign(h,vector<bool>(w,0));
         int t;
@@ -32,21 +38,7 @@ int main(void){
             for(int j = 0;j < w;j++){
                 if(island[i][j] == 1 && visited[i][j] == 0){
                     count++;
-                    togo.push({i,j});
-                    visited[i][j] = 1;
-                    while(!togo.empty()){
-                        int x = togo.front().first;
-                        int y = togo.front().second;
-                        togo.pop();
-                        for(int i = 0; i < 8;i++){
-                            if(x+tx[i] >= 0 && x+tx[i] < h && y+ty[i] >= 0 && y+ty[i] < w){
-                                if(island[x+tx[i]][y+ty[i]] == 1 && visited[x+tx[i]][y+ty[i]] == 0){
-                                    visited[x+tx[i]][y+ty[i]] = 1;
-                                    togo.push({x+tx[i],y+ty[i]});
-                                }
-                            }
-                        }
-                    }
+                    dfs(i,j);
                 }
             }
         }
