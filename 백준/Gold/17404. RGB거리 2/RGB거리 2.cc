775e2f2bd489d94391f0c007dede89d69ce6eda1
wasdfg <leejh9711@naver.com>
2024-08-13 14:36:48 +0900

    [Gold IV] Title: RGB거리 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
@@ -1,36 +1,37 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
 int main(void){
-    int N,ans = 987654321;
-    cin>>N;
-    vector<vector<int>> RGB(N,vector<int>(3,0));
-    vector<vector<int>> dp(N,vector<int>(3,0));
-    for(int i = 0;i < N;i++){
-        cin>>RGB[i][0]>>RGB[i][1]>>RGB[i][2];
+    int n,result = 1000000000;
+    cin>>n;
+    vector<vector<int>> rgb(n,vector<int>(3,0));
+    vector<vector<int>> dp(n,vector<int>(3,0));
+    for(int i = 0;i < n;i++){
+        cin>>rgb[i][0]>>rgb[i][1]>>rgb[i][2];
     }
     for(int a = 0;a < 3;a++){
-        for(int i = 0; i < 3; i++){
-			if(i == a) 
-				dp[0][i] = RGB[0][i];
-			else 
-				dp[0][i] = 987654321;
-		}
-        for(int i = 1;i < N;i++){
-            dp[i][0] = min(dp[i-1][1],dp[i-1][2])+RGB[i][0];
-            dp[i][1] = min(dp[i-1][0],dp[i-1][2])+RGB[i][1];
-            dp[i][2] = min(dp[i-1][0],dp[i-1][1])+RGB[i][2];
-        }
-        for (int i = 0; i <= 2; i++){
-			if(i == a){ 
-                continue;
+        for(int b = 0;b < 3;b++){
+            if(b == a){
+                dp[0][b] = rgb[0][b];
             }
-			else 
-                ans = min(ans, dp[N-1][i]);
-		}
+            else{
+                dp[0][b] = 987654321;
+            }
+        }
+        for(int i = 1;i < n;i++){
+            dp[i][0] = rgb[i][0]+min(dp[i-1][1],dp[i-1][2]);
+            dp[i][1] = rgb[i][1]+min(dp[i-1][0],dp[i-1][2]);
+            dp[i][2] = rgb[i][2]+min(dp[i-1][1],dp[i-1][0]);
+        }
+        for(int i = 0;i < 3;i++){
+            if(i != a){
+                result = min(result,dp[n-1][i]);
+            }
+        }
     }
-    cout<<ans;
+    cout<<result;
     return 0;
 }
