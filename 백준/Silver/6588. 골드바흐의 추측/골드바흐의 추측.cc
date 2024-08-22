14490baadc6950595865baec8ff8d0aa6de468d1
wasdfg <leejh9711@naver.com>
2024-08-22 14:25:16 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 116 ms, Memory: 2264 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
@@ -3,36 +3,38 @@
 
 using namespace std;
 
-vector<bool> check(1000001,1);
-
 int main(void){
-    int n;
-    bool notprime = false;
-    cin.tie(NULL);
     ios::sync_with_stdio(false);
-    check[0] = 0;
-    check[1] = 0;
-    for(int i = 2;i < 1001;i++){
-        for(int j = 2;j * i < 1000001;j++){
-            check[i*j] = 0;
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    vector<bool> prime(1000001,1);
+    prime[0] = 0;
+    prime[1] = 0;
+    for(int i = 2;i < 1000001;i++){
+        for(int j = 2;i*j < 1000001;j++){
+            prime[i*j] = 0;
         }
     }
     while(1){
+        bool notprime = false;
         cin>>n;
         if(n == 0){
             break;
         }
         else{
-            for(int i = n-1;i >= 2;i--){
-                if(check[i] == true && check[n-i] == true){
-                    cout<<n<<" = "<<n-i<<" + "<<i<<"\n";
+            for(int i = 2;i < n;i++){
+                if(prime[i] == 1 && prime[n-i] == 1){
+                    notprime = true;
+                    cout<<n<<" = "<<i<<" + "<<n-i<<"\n";
                     break;
                 }
             }
-            if(notprime == true){
+            if(notprime == false){
                 cout<<"Goldbach's conjecture is wrong.\n";
             }
         }
     }
+    
     return 0;
 }
