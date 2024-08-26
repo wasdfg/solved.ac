3c86ca612284021117d7cfbb8532021cfd90ff3a
wasdfg <leejh9711@naver.com>
2024-08-26 15:18:59 +0900

    [Silver I] Title: 영역 구하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.cc
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.cc
@@ -4,50 +4,49 @@
 
 using namespace std;
 
-vector<vector<bool>> monun;
-vector<vector<bool>> visited;
-int n,m,check;
-int dx[4] = {0,0,-1,1};
-int dy[4] = {1,-1,0,0};
-
-int dfs(int x,int y){
-    check++;
-    visited[x][y] = 1;
-    for(int i = 0;i < 4;i++){
-        if(x+dx[i] >= 0 && x+dx[i] < m && y+dy[i] >= 0 && y+dy[i] < n && monun[x+dx[i]][y+dy[i]] == 0 && visited[x+dx[i]][y+dy[i]] == 0){
-            dfs(x+dx[i],y+dy[i]);
-        }
-    }
-    return 0;
-}
-
 int main(void){
-    int k,x1,y1,x2,y2;
-    priority_queue<int,vector<int>,greater<int>> area;
-    cin>>m>>n>>k;
-    monun.assign(m,vector<bool>(n,0));
-    visited.assign(m,vector<bool>(n,0));
+    int n,m,check,k,x1,y1,x2,y2;
+    int dx[4] = {0,0,-1,1};
+    int dy[4] = {1,-1,0,0};
+    cin>>n>>m>>k;
+    vector<vector<int>> monun(n,vector<int>(m,1));
+    vector<vector<bool>> visited(n,vector<bool>(m,0));
+    priority_queue<int,vector<int>,greater<int>> result;
+    queue<pair<int,int>> togo;
     for(int i = 0;i < k;i++){
         cin>>x1>>y1>>x2>>y2;
         for(int a = y1;a < y2;a++){
             for(int b = x1;b < x2;b++){
-                monun[a][b] = 1;
+                monun[a][b] = 0;
+            } 
+        }
+    }
+    for(int i = 0;i < n;i++){
+        for(int j = 0;j < m;j++){
+            if(monun[i][j] == 1 && visited[i][j] == 0){
+                int area = 1;
+                visited[i][j] = 1;
+                togo.push({i,j});
+                while(!togo.empty()){
+                    int x = togo.front().first;
+                    int y = togo.front().second;
+                    togo.pop();
+                    for(int a = 0;a < 4;a++){
+                        if(x+dx[a] >= 0 && x+dx[a] < n && y+dy[a] >= 0 && y+dy[a] < m && monun[x+dx[a]][y+dy[a]] == 1 && visited[x+dx[a]][y+dy[a]] == 0){
+                            area++;
+                            visited[x+dx[a]][y+dy[a]] = 1;
+                            togo.push({x+dx[a],y+dy[a]});
+                        }
+                    }
+                }
+                result.push(area);
             }
         }
     }
-    for(int i = 0;i < m;i++){
-        for(int j = 0;j < n;j++){
-            if(monun[i][j] == 0 && visited[i][j] == 0){
-                check = 0;
-                dfs(i,j);
-                area.push(check);
-            }
-        }
-    }
-    cout<<area.size()<<endl;
-    while(!area.empty()){
-        cout<<area.top()<<" ";
-        area.pop();
+    cout<<result.size()<<"\n";
+    while(!result.empty()){
+        cout<<result.top()<<" ";
+        result.pop();
     }
     return 0;
 }
