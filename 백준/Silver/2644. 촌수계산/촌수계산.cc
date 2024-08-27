eb5fc97fb22f6bb68f1212f2bd1d4c2e8b39df07
wasdfg <leejh9711@naver.com>
2024-08-27 14:31:09 +0900

    [Silver II] Title: 촌수계산, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.cc
+++ 백준/Silver/2644. 촌수계산/촌수계산.cc
@@ -8,39 +8,29 @@ vector<int> chon[101];
 vector<int> check(101,0);
 vector<bool> visited(101,0);
 
+int n,s,e,m,x,y;
+
+void dfs(int now){
+    for(int i = 0;i < chon[now].size();i++){
+        if(visited[chon[now][i]] == 0){
+            visited[chon[now][i]] = 1;
+            check[chon[now][i]] = check[now]+1;
+            dfs(chon[now][i]);
+        }
+    }
+}
+
 int main(void){
-    int n,s,e,m,x,y;
-    bool nochon = true;
-    queue<int> togo;
     cin>>n>>s>>e>>m;
     for(int i = 0;i < m;i++){
         cin>>x>>y;
         chon[x].push_back(y);
         chon[y].push_back(x);
     }
-    for(int i = 0;i < 101;i++){
-        sort(chon[i].begin(),chon[i].end());
-    }
-    togo.push(s);
     visited[s] = 1;
-    check[s] = 1;
-    while(!togo.empty()){
-        int a = togo.front();
-        togo.pop();
-        if(a == e){
-            nochon = false;
-            break;
-        }
-        for(int i = 0;i < chon[a].size();i++){
-            if(visited[chon[a][i]] == false){
-                visited[chon[a][i]] = true;
-                check[chon[a][i]] = check[a]+1;
-                togo.push(chon[a][i]);
-            }
-        }
-    }
-    if(nochon == false){
-        cout<<check[e]-1;
+    dfs(s);
+    if(check[e] != 0){
+        cout<<check[e];
     }
     else{
         cout<<"-1";
