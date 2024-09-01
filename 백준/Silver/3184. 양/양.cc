efcaf145295ec714f1c5e0db922603df9e5b89d9
wasdfg <leejh9711@naver.com>
2024-09-01 14:15:11 +0900

    [Silver I] Title: 양, Time: 4 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Silver/3184. 양/양.cc
+++ 백준/Silver/3184. 양/양.cc
@@ -1,43 +1,25 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 
 using namespace std;
 
-vector<vector<int>> yard;
-vector<vector<bool>> visited;
-int R,C,sheep,wolf;
-int dx[4] = {0,0,-1,1};
-int dy[4] = {1,-1,0,0};
-
-int dfs(int x,int y){
-    visited[x][y] = 1;
-    if(yard[x][y] == 2){
-        sheep++;
-    }
-    if(yard[x][y] == 3){
-        wolf++;
-    }
-    for(int i = 0;i < 4;i++){
-        if(x+dx[i] >= 0 && x+dx[i] < R && y+dy[i] >= 0 && y+dy[i] < C && visited[x+dx[i]][y+dy[i]] == 0 && yard[x+dx[i]][y+dy[i]] != 1){
-            dfs(x+dx[i],y+dy[i]);
-        }
-    }
-    return 0;
-}
-
 int main(void){
-    int lives = 0,livew = 0;
+    int r,c,sheep,wolf;
+    int dx[4] = {0,0,-1,1};
+    int dy[4] = {1,-1,0,0};
+    int livesheep = 0,livewolf = 0;
     char a;
-    cin>>R>>C;
-    yard.assign(R,vector<int>(C,0));
-    visited.assign(R,vector<bool>(C,0));
-    for(int i = 0;i < R;i++){
-        for(int j = 0;j < C;j++){
+    cin>>r>>c;
+    vector<vector<int>> yard(r,vector<int>(c,0));
+    vector<vector<bool>> visited(r,vector<bool>(c,0));
+    for(int i = 0;i < r;i++){
+        for(int j = 0;j < c;j++){
             cin>>a;
-            if(a == '.'){
+            if(a == '#'){
                 yard[i][j] = 0;
             }
-            else if(a == '#'){
+            else if(a == '.'){
                 yard[i][j] = 1;
             }
             else if(a == 'o'){
@@ -48,21 +30,46 @@ int main(void){
             }
         }
     }
-    for(int i = 0;i < R;i++){
-        for(int j = 0;j < C;j++){
-            sheep = 0;
-            wolf = 0;
-            if(yard[i][j] != 1 && visited[i][j] == 0){
-                dfs(i,j);
+    for(int i = 0;i < r;i++){
+        for(int j = 0;j < c;j++){
+            if(yard[i][j] >= 2 && visited[i][j] == 0){
+                sheep = 0;
+                wolf = 0;
+                queue<pair<int,int>> togo;
+                visited[i][j] = 1;
+                togo.push({i,j});
+                if(yard[i][j] == 2){
+                    sheep++;
+                }
+                else{
+                    wolf++;
+                }
+                while(!togo.empty()){
+                    int x = togo.front().first;
+                    int y = togo.front().second;
+                    togo.pop();
+                    for(int t = 0;t < 4;t++){
+                        if(x+dx[t] >= 0 && x+dx[t] < r && y+dy[t] >= 0 && y+dy[t] < c && yard[x+dx[t]][y+dy[t]] > 0 && visited[x+dx[t]][y+dy[t]] == 0){
+                            visited[x+dx[t]][y+dy[t]] = 1;
+                            if(yard[x+dx[t]][y+dy[t]] == 2){
+                                sheep++;
+                            }
+                            else if(yard[x+dx[t]][y+dy[t]] == 3){
+                                wolf++;
+                            }
+                            togo.push({x+dx[t],y+dy[t]});
+                        }
+                    }
+                }
                 if(sheep > wolf && !(wolf == 0 && sheep == 0)){
-                    lives+= sheep;
+                    livesheep+= sheep;
                 }
                 else if(sheep <= wolf && !(wolf == 0 && sheep == 0)){
-                    livew+= wolf;
+                    livewolf+= wolf;
                 }
             }
         }
     }
-    cout<<lives<<" "<<livew;
+    cout<<livesheep<<" "<<livewolf;
     return 0;
 }
