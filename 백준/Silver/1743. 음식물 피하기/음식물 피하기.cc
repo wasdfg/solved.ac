988fa3654b4dd494be6f0e12fa3b05a01d06a88d
wasdfg <leejh9711@naver.com>
2024-09-02 14:25:49 +0900

    [Silver I] Title: 음식물 피하기, Time: 0 ms, Memory: 2136 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.cc
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.cc
@@ -4,40 +4,37 @@
 
 using namespace std;
 
+int n,m,k,r,c,count,result = 0;
+int dx[4] = {0,0,-1,1};
+int dy[4] = {-1,1,0,0};
+vector<vector<int>> trash;
+vector<vector<bool>> visited;
+
+void dfs(int x,int y){
+    for(int i = 0;i < 4;i++){
+        if(x+dx[i] >= 0 && x+dx[i] < n && y+dy[i] >= 0 && y+dy[i] < m && trash[x+dx[i]][y+dy[i]] == 1 && visited[x+dx[i]][y+dy[i]] == 0){
+            visited[x+dx[i]][y+dy[i]] = 1;
+            count++;
+            dfs(x+dx[i],y+dy[i]);
+        }
+    }
+}
+
 int main(void){
-    int n,m,k,r,c,count,result = 0;
-    int dx[4] = {0,0,-1,1};
-    int dy[4] = {-1,1,0,0};
     cin>>n>>m>>k;
-    vector<vector<int>> trash(n,vector<int>(m,0));
-    vector<vector<bool>> visited(n,vector<bool>(m,0));
+    trash.assign(n,vector<int>(m,0));
+    visited.assign(n,vector<bool>(m,0));
     for(int i = 0;i < k;i++){
         cin>>r>>c;
         trash[r-1][c-1] = 1;
     }
-    priority_queue<pair<int,int>> togo;
     for(int i = 0;i < n;i++){
         for(int j = 0;j < n;j++){
             if(trash[i][j] == 1 && visited[i][j] == 0){
-                count = 0;
+                count = 1;
                 visited[i][j] = 1;
-                togo.push({i,j});
-                count++;
-                while(!togo.empty()){
-                    int x = togo.top().first;
-                    int y = togo.top().second;
-                    togo.pop();
-                    for(int k = 0;k < 4;k++){
-                        if(x+dx[k] >= 0 && x+dx[k] < n && y+dy[k] >= 0 && y+dy[k] < m && trash[x+dx[k]][y+dy[k]] == 1 && visited[x+dx[k]][y+dy[k]] == 0){
-                            count++;
-                            visited[x+dx[k]][y+dy[k]] = 1;
-                            togo.push({x+dx[k],y+dy[k]});
-                        }
-                    }
-                }
-                if(count > result){
-                    result = count;
-                }
+                dfs(i,j);
+                result = max(count,result);
             }
         }
     }
