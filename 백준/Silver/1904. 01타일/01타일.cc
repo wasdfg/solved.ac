b6fe36347fd3fe59a06772949375ad052377244e
wasdfg <leejh9711@naver.com>
2024-09-06 13:50:42 +0900

    [Silver III] Title: 01타일, Time: 8 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.cc
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -7,21 +7,11 @@ int main(void){
     int n;
     cin>>n;
     vector<int> zo(n,0);
-    if(n > 2){
-        zo[0] = 1;
-        zo[1] = 2;
-        for(int i = 2;i < n;i++){
-            zo[i] = ((zo[i-1] % 15746) + (zo[i-2] % 15746)) % 15746;
-        }
-        cout<<zo[n-1] % 15746;
+    zo[0] = 1;
+    zo[1] = 2;
+    for(int i = 2;i < n;i++){
+        zo[i] = zo[i-1] % 15746 + zo[i-2] % 15746;
     }
-    else{
-        if(n == 1){
-            cout<<"1";
-        }
-        else{
-            cout<<"2";
-        }
-    }
+    cout<<zo[n-1] % 15746;
     return 0;
 }
