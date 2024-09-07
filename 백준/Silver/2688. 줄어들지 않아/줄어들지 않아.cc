da63f36294d6cd5770a6d710653b07d68f3301ca
wasdfg <leejh9711@naver.com>
2024-09-07 15:24:14 +0900

    [Silver I] Title: 줄어들지 않아, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2688. 줄어들지 않아/줄어들지 않아.cc
+++ 백준/Silver/2688. 줄어들지 않아/줄어들지 않아.cc
@@ -4,17 +4,20 @@
 using namespace std;
 
 int main(void){
-    long long t,n,sum = 0;
+    long long t,n,sum = 0,check;
     vector<vector<long long>> dp(64,vector<long long>(10,0));
     cin>>t;
     for(int i = 0;i < 10;i++){
         dp[0][i] = 1;
+        sum+=dp[0][i];
     }
     for(int i = 1;i < 64;i++){
+        check = sum;
+        sum = 0;
         for(int j = 0;j < 10;j++){
-            for(int k = j;k <= 9;k++){
-                dp[i][j] += dp[i-1][k];
-            }
+            dp[i][j] = check;
+            sum+=check;
+            check-=dp[i-1][j];   
         }
     }
     for(int i = 0;i < t;i++){
