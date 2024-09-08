520a8650ef72bc2dae9386dfd6b8921558241fcd
wasdfg <leejh9711@naver.com>
2024-09-08 15:52:04 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 32 ms, Memory: 5404 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -16,24 +16,25 @@ int main(void){
         int cost = togo.front().second;
         togo.pop();
         visited[now] = 1;
-        if(count != 0 && now == e && cost == check){
-            count++;
+        if(now == e){
+            if(count != 0 && cost == count){
+                check++;
+            }
+            if(count == 0){
+                count = cost;
+                check = 1;
+            }
         }
-        if(count == 0 && now == e){
-            check = cost;
-            count++;
-        }
-        if(now*2 <= 100000 && visited[now*2] == 0){
-            togo.push({now*2,cost+1});
-        }
-        if(now+1 <= 100000 && visited[now+1] == 0){
+        if(now + 1 <= 100000 && visited[now+1] == 0){
             togo.push({now+1,cost+1});
         }
-        if(now-1 >= 0 && visited[now-1] == 0){
+        if(now - 1 >= 0 && visited[now-1] == 0){
             togo.push({now-1,cost+1});
         }
+        if(now * 2 <= 100000 && visited[now*2] == 0){
+            togo.push({now*2,cost+1});
+        }
     }
-    cout<<check<<endl;
-    cout<<count;
+    cout<<count<<"\n"<<check;
     return 0;
 }
