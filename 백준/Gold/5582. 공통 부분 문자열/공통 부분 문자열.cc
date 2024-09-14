ea556b0fd1a037c17de9bf53803506239e20a56f
wasdfg <leejh9711@naver.com>
2024-09-14 17:45:55 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 80 ms, Memory: 64660 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.cc
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.cc
@@ -4,25 +4,21 @@
 
 using namespace std;
 
-vector<vector<int>> lcs;
-
 int main(void){
     int result = 0;
     string a,b;
     cin>>a>>b;
-    lcs.assign(a.length()+1,vector<int>(b.length()+1,0));
-    for(int i = 1;i <= a.length();i++){
-        for(int j = 1;j <= b.length();j++){
-            if(a[i-1] == b[j-1]){
-                lcs[i][j] = lcs[i-1][j-1]+1;
+    vector<vector<int>> lcs(a.length()+1,vector<int>(b.length()+1,0));
+    for(int i = 0;i < a.length();i++){
+        for(int j = 0;j < b.length();j++){
+            if(a[i] == b[j]){
+                lcs[i+1][j+1] = lcs[i][j]+1;
             }
         }
     }
-    for(int i = 1;i <= a.length();i++){
-        for(int j = 1;j <= b.length();j++){
-            if(lcs[i][j] > result){
-                result = lcs[i][j];
-            }
+    for(int i = 1;i < a.length()+1;i++){
+        for(int j = 1;j < b.length()+1;j++){
+            result = max(result,lcs[i][j]);
         }
     }
     cout<<result;
