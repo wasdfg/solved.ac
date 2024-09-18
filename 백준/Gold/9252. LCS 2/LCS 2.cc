c68a1a02a720ff63d9ec1e6ae48f0f722f3138ac
wasdfg <leejh9711@naver.com>
2024-09-18 15:48:51 +0900

    [Gold IV] Title: LCS 2, Time: 4 ms, Memory: 5984 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.cc
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -8,28 +8,27 @@ using namespace std;
 int main(void){
     int check = 0,x,y;
     string a,b;
-    string answer = "";
+    string result;
     cin>>a>>b;
-    x = a.length();
-    y = b.length();
-    vector<vector<int>> lcs(a.length()+1,vector<int>(b.length()+1,0));
-    for(int i = 1;i <= a.length();i++){
-        for(int j = 1;j <= b.length();j++){
+    x = a.length()+1;
+    y = b.length()+1;
+    vector<vector<int>> lcs(x,vector<int>(y,0));
+    for(int i = 1;i < x;i++){
+        for(int j = 1;j < y;j++){
             if(a[i-1] == b[j-1]){
                 lcs[i][j] = lcs[i-1][j-1]+1;
-                if(lcs[i][j] > check){
-                    check = lcs[i][j];
-                }
             }
             else{
                 lcs[i][j] = max(lcs[i-1][j],lcs[i][j-1]);
-                if(lcs[i][j] > check){
-                    check = lcs[i][j];
-                }
             }
         }
     }
-    while(lcs[x][y] != 0){
+    x--;
+    y--;
+    while(1){
+        if(lcs[x][y] == 0){
+            break;
+        }
         if(lcs[x][y] == lcs[x-1][y]){
             x--;
         }
@@ -37,13 +36,16 @@ int main(void){
             y--;
         }
         else{
-            answer+=a[x-1];
+            check++;
+            result+=a[x-1];
             x--;
             y--;
         }
     }
-    reverse(answer.begin(),answer.end());
     cout<<check<<endl;
-    cout<<answer;
+    if(check != 0){
+        reverse(result.begin(),result.end());
+        cout<<result;
+    }
     return 0;
 }
