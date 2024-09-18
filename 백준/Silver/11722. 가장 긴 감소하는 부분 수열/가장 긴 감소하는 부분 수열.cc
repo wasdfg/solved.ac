061e62224152daeccc921423cada55991f0ab4ac
wasdfg <leejh9711@naver.com>
2024-09-18 14:28:40 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -8,29 +8,16 @@ int main(void){
     int n,num;
     cin>>n;
     vector<int> box;
-    vector<int> lis;
-    box.push_back(1001);
-    lis.push_back(0);
-    for(int i = 1;i <= n;i++){
+    for(int i = 0;i < n;i++){
         cin>>num;
-        if(num < box.back()){
+        if(box.empty() || box.back() > num){
             box.push_back(num);
-            lis.push_back(box.size()-1);
         }
         else{
-            for(int j = 1;j <= box.size();j++){
-                if(box[j] == num){
-                    lis.push_back(j);
-                    break;
-                }
-                else if(box[j] < num){
-                    box[j] = num;
-                    lis.push_back(j);
-                    break;
-                }
-            }
+            int x = lower_bound(box.begin(),box.end(),num,greater<int>()) - box.begin();
+            box[x] = num;
         }
     }
-    cout<<*max_element(lis.begin(),lis.end());
+    cout<<box.size();
     return 0;
 }
