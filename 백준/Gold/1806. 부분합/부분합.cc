7b143ba0e8e0fcea51be6827b7c0f8abf95b906a
wasdfg <leejh9711@naver.com>
2024-09-20 14:12:22 +0900

    [Gold IV] Title: 부분합, Time: 28 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(void){
-    int start = 0,end = 0,n,s,check = 100000,result;
+    int start = 0,end = 0,n,s,result,check = 100001;
     cin>>n>>s;
     vector<int> nsum(n,0);
     for(int i = 0;i < n;i++){
@@ -13,9 +13,7 @@ int main(void){
     result = nsum[0];
     while(start <= end && end < n){
         if(result >= s){
-            if(check > end-start+1){
-                check = end-start+1;
-            }
+            check = min(end-start+1,check);
         }
         if(result < s){
             end++;
@@ -23,14 +21,14 @@ int main(void){
         }
         else{
             result-=nsum[start];
-            start++;
+            start++;     
         }
     }
-    if(check != 100000){
+    if(check != 100001){
         cout<<check;
     }
     else{
-        cout<<0;
+        cout<<"0";
     }
     return 0;
 }
