a1813cc3abe0427de91e32f327585c0899ab573f
wasdfg <leejh9711@naver.com>
2024-09-24 14:13:35 +0900

    [Gold V] Title: 회문, Time: 88 ms, Memory: 2556 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.cc
+++ 백준/Gold/17609. 회문/회문.cc
@@ -1,32 +1,31 @@
 #include<iostream>
-#include<vector>
 #include<string>
 
 using namespace std;
 
-string a;
-
-int pal(int start,int end,bool usa){
-    while(start < end){
-        if(a[start] != a[end]){
-            if(usa){
-                if(pal(start+1, end, 0) == 0 || pal(start, end-1, 0) == 0){
+int palindrome(int left,int right,string s,bool removeone){
+    while(left < right){
+        if(s[left] != s[right]){
+            if(removeone == false){
+                if(palindrome(left,right-1,s,1) == 0 || palindrome(left+1,right,s,1) == 0){
                     return 1;
-                } 
+                }
             }
             return 2;
         }
-        start++,end--;
+        left++;
+        right--;
     }
     return 0;
 }
 
 int main(void){
-    int n,result;
+    int n;
+    string s;
     cin>>n;
     for(int i = 0;i < n;i++){
-        cin>>a;
-        cout<<pal(0,a.length()-1,1)<<endl;
+        cin>>s;
+        cout<<palindrome(0,s.length()-1,s,0)<<endl;
     }
     return 0;
 }
