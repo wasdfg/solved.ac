79847eea4a6de6a9ab7ac95b71b85cdcc5b45ea3
wasdfg <leejh9711@naver.com>
2024-09-25 14:50:59 +0900

    [Gold V] Title: 수 고르기, Time: 60 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.cc
+++ 백준/Gold/2230. 수 고르기/수 고르기.cc
@@ -5,32 +5,26 @@
 using namespace std;
 
 int main(void){
-    int start,end,check,n,m;
+    int start = 0,end = 0,check = 2000000001,n,m;
     cin>>n>>m;
     vector<int> choose(n,0);
     for(int i = 0;i < n;i++){
         cin>>choose[i];
     }
     sort(choose.begin(),choose.end());
-    start = 0,end = 0;
-    check = 2000000001;
-    while(end < n){
+    while(start <= end && end < n){
         if(choose[end] - choose[start] >= m){
-            check = min(check, choose[end] - choose[start]);
-            if(check == m){
-                break;
+            if(check >= choose[end] - choose[start]){
+                check = choose[end] - choose[start];
             }
+        }
+        if(choose[end] - choose[start] >= m){
             start++;
         }
         else{
             end++;
         }
     }
-    if(n == 1){
-        cout<<choose.front();
-    }
-    else{
-        cout<<check;
-    }
+    cout<<check;
     return 0;
 }
