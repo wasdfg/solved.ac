d26abd49f073e4aa5963537939435b0efe386cd5
wasdfg <leejh9711@naver.com>
2024-09-29 13:53:03 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 568 ms, Memory: 8176 KB -BaekjoonHub

--- 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
@@ -6,36 +6,16 @@ using namespace std;
 
 int main(void){
     int n,num;
-    cin>>n;
-    vector<int> box(n,0);
     vector<int> lis;
+    cin>>n;
     for(int i = 0;i < n;i++){
-        cin>>box[i];
-    }
-    for(int i = 0;i < n;i++){
-        if(lis.empty()){
-            lis.push_back(box[i]);
+        cin>>num;
+        if(lis.empty() || lis.back() < num){
+            lis.push_back(num);
         }
         else{
-            if(lis.back() < box[i]){
-                lis.push_back(box[i]);
-            }
-            else{
-                //auto it = lower_bound(lis.begin(),lis.end(),box[i]);
-                //*it = box[i];
-                int left = 0;
-                int right = lis.size()-1;
-                while(left < right){
-                    int mid = (left+right)/2;
-                    if(lis[mid] >= box[i]){
-                        right = mid;
-                    }
-                    else{
-                        left = mid+1;
-                    }
-                }
-                lis[left] = box[i];
-            }
+            int x = lower_bound(lis.begin(),lis.end(),num)-lis.begin();
+            lis[x] = num;
         }
     }
     cout<<lis.size();
