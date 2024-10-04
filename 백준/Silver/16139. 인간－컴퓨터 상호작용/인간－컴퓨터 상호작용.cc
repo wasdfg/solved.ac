95eb0eccb8d03342982fd9f264005c622daba6b8
wasdfg <leejh9711@naver.com>
2024-10-04 15:23:46 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 52 ms, Memory: 2160 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+int main(void){
+    string s;
+    int q,l,r;
+    char a;
+    cin>>s>>q;
+    vector<vector<int>> word(26,vector<int>(s.length()+1,0));
+    for(int i = 0;i < s.length();i++){
+        if(i != 0){
+            for(int j = 0;j < 26;j++){
+                word[j][i]+=word[j][i-1];
+            }
+        }
+        word[s[i]-'a'][i]++;
+    }
+    for(int i = 0;i < q;i++){
+        cin>>a>>l>>r;
+        if(l == 0){
+            cout<<word[a-'a'][r]<<"\n";
+        }
+        else{
+            cout<<word[a-'a'][r]-word[a-'a'][l-1]<<"\n";
+        }
+    }
+    return 0;
+}
