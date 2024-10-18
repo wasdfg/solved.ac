02ede01bc51612138a08537d856b07b91664c1e9
wasdfg <leejh9711@naver.com>
2024-10-18 14:54:39 +0900

    [Gold V] Title: 용액 합성하기, Time: 48 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/14921. 용액 합성하기/용액 합성하기.cc
+++ 백준/Gold/14921. 용액 합성하기/용액 합성하기.cc
@@ -1,31 +1,36 @@
 #include<iostream>
 #include<vector>
-#include<cmath>
+#include<algorithm>
 
 using namespace std;
 
 int main(void){
-    int n,check = 200000001,left,right,l,r;
+    int n,left,right,result = 200000001;
     cin>>n;
     vector<int> liq(n,0);
     for(int i = 0;i < n;i++){
         cin>>liq[i];
     }
-    left = 0;
-    right = n-1;
+    sort(liq.begin(),liq.end());
+    left = 0,right = n-1;
     while(left < right){
-        if(abs(liq[left]+liq[right]) < check){
-            check = abs(liq[left]+liq[right]);
-            l = left;
-            r = right;
+        if(liq[left]+liq[right] == 0){
+            result = 0;
+            break;
         }
-        if(liq[left]+liq[right] > 0){
+        else if(liq[left]+liq[right] > 0){
+            if(abs(result) > abs(liq[left]+liq[right])){
+                result = liq[left]+liq[right];
+            }
             right--;
         }
         else{
+            if(abs(result) > abs(liq[left]+liq[right])){
+                result = liq[left]+liq[right];
+            }
             left++;
         }
     }
-    cout<<liq[l]+liq[r];
+    cout<<result;
     return 0;
 }
