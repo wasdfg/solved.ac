038254fe6cf4e53186ba4a361dac654718e5e50f
wasdfg <leejh9711@naver.com>
2024-10-19 15:13:27 +0900

    [Silver I] Title: 귀여운 라이언, Time: 156 ms, Memory: 5908 KB -BaekjoonHub

--- 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.cc
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.cc
@@ -4,26 +4,33 @@
 using namespace std;
 
 int main(void){
-    int n,k,x,count = 0,left = 0,right,len;
+    int n,k,left = 0,right = 0,count = 0,check;
     cin>>n>>k;
-    vector<int> rion;
+    vector<int> rion(n,0);
     for(int i = 0;i < n;i++){
-        cin>>x;
-        if(x == 1){
-            rion.push_back(i+1);
+        cin>>rion[i];
+    }
+    check = n+1;
+    while(right < n){
+        while(count < k && right < n){
+            if(rion[right] == 1){
+                count++;
+            }
+            right++;
+        }
+        while(count == k){
+            check = min(right-left,check);
+            if(rion[left] == 1){
+                count--;
+            }
+            left++;
         }
     }
-    if(rion.size() < k){
+    if(check == n+1){
         cout<<"-1";
-        return 0;
     }
-    len = n+1;
-    right = k-1;
-    for(int i = 0;i <= rion.size()-k;i++){
-        len = min(len,rion[right]-rion[left]+1);
-        right++;
-        left++;
+    else{
+        cout<<check;
     }
-    cout<<len;
     return 0;
 }
