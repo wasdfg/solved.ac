c5d4ddb54fce45bcbb744b82c4f364272e9328bd
wasdfg <leejh9711@naver.com>
2024-10-24 15:04:11 +0900

    [Gold III] Title: 파티, Time: 84 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -4,65 +4,47 @@
 
 using namespace std;
 
-vector<pair<int,int>> route[1001];
-int N,M,X;
+vector<pair<int,int>> road[1000];
+vector<int> pos;
+int n,m,x,check;
+
+void daijk(int start,int end){
+    pos.assign(n,100000001);
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+    pq.push({0,start});
+    pos[start] = 0;
+    while(!pq.empty()){
+        int now = pq.top().second;
+        int cost = pq.top().first;
+        pq.pop();
+        if(now == end){
+            check = pos[now];
+            break;
+        }
+        for(int i = 0;i < road[now].size();i++){
+            int next = road[now][i].first;
+            int ncost = road[now][i].second;
+            if(pos[next] > cost+ncost){
+                pos[next] = cost+ncost;
+                pq.push({cost+ncost,next});
+            }
+        }
+    }
+}
 
 int main(void){
-    int a,b,c,toX,tohome,result = 0;
-    cin>>N>>M>>X;
-    vector<int> home(N+1,987654321);
-    for(int i = 0;i < M;i++){
+    int a,b,c,tox,tohome,result = 0;
+    cin>>n>>m>>x;
+    for(int i = 0;i < m;i++){
         cin>>a>>b>>c;
-        route[a].push_back({b,c});
+        road[a-1].push_back({b-1,c});
     }
-    for(int i = 1;i <= N;i++){
-        home.assign(N+1,987654321);
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        pq.push({0,i});
-        home[i] = 0;
-        while(!pq.empty()){
-            int cost = pq.top().first;
-            int now = pq.top().second;
-            pq.pop();
-            if(now == X){
-                toX = home[now];
-                break;
-            }
-            for(int i = 0;i < route[now].size();i++){
-                int ncost = cost+route[now][i].second;
-                int next = route[now][i].first;
-                if(ncost < home[next]){
-                    home[next] = ncost;
-                    pq.push({ncost,next});
-                }
-            }
-        }
-        while(!pq.empty()){
-            pq.pop();
-        }
-        home.assign(N+1,987654321);
-        pq.push({0,X});
-        home[X] = 0;
-        while(!pq.empty()){
-            int cost = pq.top().first;
-            int now = pq.top().second;
-            pq.pop();
-            if(now == i){
-                tohome = home[now];
-                break;
-            }
-            for(int i = 0;i < route[now].size();i++){
-                int ncost = cost+route[now][i].second;
-                int next = route[now][i].first;
-                if(ncost < home[next]){
-                    home[next] = ncost;
-                    pq.push({ncost,next});
-                }
-            }
-        }
-        if(toX+tohome > result){
-            result = toX+tohome;
-        }
+    for(int i = 0;i < n;i++){
+        daijk(i,x-1);
+        tox = check;
+        daijk(x-1,i);
+        tohome = check;
+        result = max(result,tox+tohome);
     }
     cout<<result;
     return 0;
