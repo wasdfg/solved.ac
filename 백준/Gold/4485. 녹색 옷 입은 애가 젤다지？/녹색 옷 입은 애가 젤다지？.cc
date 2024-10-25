43ae10546de0bdaeaa86c8da4d039ed28e968fc3
wasdfg <leejh9711@naver.com>
2024-10-25 14:58:44 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 4 ms, Memory: 2276 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
@@ -10,39 +10,38 @@ int main(void){
     int n,a = 1;
     int dx[4] = {0,0,-1,1};
     int dy[4] = {1,-1,0,0};
-    vector<vector<int>> br;
-    vector<vector<int>> dist;
     while(1){
-        cin>>n;
-        if(n == 0){
-            return 0;
-        }
-        else{
-            br.assign(n,vector<int>(n,0));
-            dist.assign(n,vector<int>(n,987654321));
-            for(int i = 0;i < n;i++){
-                for(int j = 0;j < n;j++){
-                    cin>>br[i][j];
-                }
+        cin>> n;
+        if(n == 0)
+            break;
+        vector<vector<int>> daijk(n,vector<int>(n,1200));
+        vector<vector<int>> zelda(n,vector<int>(n,0));
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < n;j++){
+                cin>>zelda[i][j];
             }
-            queue<pair<int,int>> pq;
-            pq.push({0,0});
-            dist[0][0] = br[0][0];
-            while(!pq.empty()){
-                int x = pq.front().first;
-                int y = pq.front().second;
-                pq.pop();
-                for(int i = 0;i < 4;i++){
-                    if(x+dx[i] >= 0 && x+dx[i] < n && y+dy[i] >= 0 && y+dy[i] < n){
-                        if(dist[x+dx[i]][y+dy[i]] > br[x+dx[i]][y+dy[i]]+dist[x][y]){
-                            dist[x+dx[i]][y+dy[i]] = br[x+dx[i]][y+dy[i]]+dist[x][y];
-                            pq.push({x+dx[i],y+dy[i]});
-                        }
+        }
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
+        daijk[0][0] = zelda[0][0];
+        pq.push({daijk[0][0],{0,0}});
+        while(!pq.empty()){
+            int cost = pq.top().first;
+            int x = pq.top().second.first;
+            int y = pq.top().second.second;
+            pq.pop();
+            if(x == n-1 && y == n-1)
+                break;
+            for(int i = 0;i < 4;i++){
+                if(x+dx[i] >= 0 && x+dx[i] < n && y+dy[i] >= 0 && y+dy[i] < n){
+                    int ncost = zelda[x+dx[i]][y+dy[i]];
+                    if(cost+ncost < daijk[x+dx[i]][y+dy[i]]){
+                        daijk[x+dx[i]][y+dy[i]] = cost+ncost;
+                        pq.push({cost+ncost,{x+dx[i],y+dy[i]}});
                     }
                 }
             }
-            cout<<"Problem "<<a<<": "<<dist[n-1][n-1]<<endl;
         }
+        cout<<"Problem "<<a<<": "<<daijk[n-1][n-1]<<"\n";
         a++;
     }
     return 0;
