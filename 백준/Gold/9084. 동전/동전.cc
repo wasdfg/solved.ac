a19449f83b517ce044c8d0f79620f363d136af27
wasdfg <leejh9711@naver.com>
2024-11-07 13:46:20 +0900

    [Gold V] Title: 동전, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.cc
+++ 백준/Gold/9084. 동전/동전.cc
@@ -8,16 +8,16 @@ int main(void){
     cin>>t;
     for(int i = 0;i < t;i++){
         cin>>n;
-        vector<int> coin(21,0);
-        vector<int> dp(10001,0);
-        for(int j = 1;j <= n;j++){
+        vector<int> coin(n,0);
+        for(int j = 0;j < n;j++){
             cin>>coin[j];
         }
         cin>>m;
+        vector<int> dp(m+1,0);
         dp[0] = 1;
-        for(int j = 1;j <= n;j++){
-            for(int k = coin[j];k <= m;k++){
-                dp[k]+=dp[k-coin[j]];
+        for(int a = 0;a < n;a++){
+            for(int b = coin[a];b < m+1;b++){
+                dp[b]+=dp[b-coin[a]];
             }
         }
         cout<<dp[m]<<endl;
