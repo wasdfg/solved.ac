5294109f9dab17337ea01e272489d0050592bec5
wasdfg <leejh9711@naver.com>
2024-11-11 15:12:40 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 52 ms, Memory: 10928 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -5,54 +5,36 @@
 
 using namespace std;
 
-bool cmp(pair<long long,long long> &a,pair<long long,long long> &b){
-    if(a.second == b.second){
-        return a.first < b.first;
-    }
-    else{
-        return a.second < b.second;
-    }
-}
-
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    long long v,e,a,b,c;
-    long long result = 1e+18;
+    long long v,e,a,b,c,result = 0;
     cin>>v>>e;
-    vector<pair<long long,long long>> graph[v+1];
-    vector<bool> visited;
+    vector<pair<long long,long long>> prim[v+1];
+    vector<bool> visited(v+1,0);
+    priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>> togo;
     for(int i = 0;i < e;i++){
         cin>>a>>b>>c;
-        graph[a].push_back({b,c});
-        graph[b].push_back({a,c});
+        prim[a].push_back({b,c});
+        prim[b].push_back({a,c});
     }
-    for(int i = 1;i < v+1;i++){
-        sort(graph[i].begin(),graph[i].end(),cmp);
-    }
-        priority_queue<pair<long long,long long>,vector<pair<long long,long long>>,greater<pair<long long,long long>>> pq;
-        for(int j = 0;j < graph[1].size();j++){
-            pq.push({graph[1][j].second,graph[1][j].first});
+    togo.push({0,1});
+    while(!togo.empty()){
+        int x = togo.top().second;
+        long long check = togo.top().first;
+        togo.pop();
+        if(visited[x] == 1){
+            continue;
         }
-        visited.assign(v+1,0);
-        long long check = 0;
-        visited[1] = 1;
-        while(!pq.empty()){
-            int cost = pq.top().first;
-            int now = pq.top().second;
-            pq.pop();
-            if(visited[now] == 0){
-                visited[now] = 1;
-                check+=cost;
-                for(int j = 0;j < graph[now].size();j++){
-                    if(visited[graph[now][j].first] == 0){
-                        pq.push({graph[now][j].second,graph[now][j].first});
-                    }
-                }
+        visited[x] = 1;
+        result+=check;
+        for(int i = 0;i < prim[x].size();i++){
+            if(visited[prim[x][i].first] == 0){
+                togo.push({prim[x][i].second,prim[x][i].first});
             }
         }
-        result = min(result,check);
+    }
     cout<<result;
     return 0;
 }
