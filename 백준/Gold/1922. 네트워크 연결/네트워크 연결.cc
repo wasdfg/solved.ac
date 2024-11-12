7591d5e8b6595a034454bb7a49c5d3350bae5b97
wasdfg <leejh9711@naver.com>
2024-11-12 15:51:18 +0900

    [Gold IV] Title: 네트워크 연결, Time: 100 ms, Memory: 6432 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<queue>
 #include<vector>
-#include<algorithm>
 
 using namespace std;
 
@@ -10,6 +9,7 @@ int main(void){
     cin>>n>>m;
     vector<pair<int,int>> com[n+1];
     vector<bool> visited(n+1,0);
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> togo;
     for(int i = 0;i < m;i++){
         cin>>a>>b>>c;
         if(a != b){
@@ -17,22 +17,19 @@ int main(void){
             com[b].push_back({a,c});
         }
     }
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-    for(int i = 0;i < com[1].size();i++){
-        pq.push({com[1][i].second,com[1][i].first});
-    }
-    visited[1] = 1;
-    while(!pq.empty()){
-        int cost = pq.top().first;
-        int now = pq.top().second;
-        pq.pop();
-        if(visited[now] == 0){
-            visited[now] = 1;
-            result += cost; 
-            for(int i = 0;i < com[now].size();i++){
-                if(visited[com[now][i].first] == 0){
-                    pq.push({com[now][i].second,com[now][i].first});
-                }
+    togo.push({0,1});
+    while(!togo.empty()){
+        int cost = togo.top().first;
+        int now = togo.top().second;
+        togo.pop();
+        if(visited[now] == 1){
+            continue;
+        }
+        result+=cost;
+        visited[now] = 1;
+        for(int i = 0;i < com[now].size();i++){
+            if(visited[com[now][i].first] == 0){
+                togo.push({com[now][i].second,com[now][i].first});
             }
         }
     }
