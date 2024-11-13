1ca19a611ae51ddbd4986d61b9031f918d8a39c1
wasdfg <leejh9711@naver.com>
2024-11-13 14:36:19 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1424 ms, Memory: 43696 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
@@ -14,23 +14,23 @@ int main(void){
         home[a].push_back({b,c});
         home[b].push_back({a,c});
     }
-    visited[1] = 1;
     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-    for(int i = 0;i < home[1].size();i++){
-        pq.push({home[1][i].second,home[1][i].first});
-    }
+    pq.push({0,1});
     while(!pq.empty()){
         int now = pq.top().second;
         int cost = pq.top().first;
         pq.pop();
-        if(visited[now] == 0){
-            visited[now] = 1;
-            result+=cost;
-            check = max(check,cost);
-            for(int i = 0;i < home[now].size();i++){
-                if(visited[home[now][i].first] == 0){
-                    pq.push({home[now][i].second,home[now][i].first});
-                }
+        if(visited[now] == 1){
+            continue;
+        }
+        visited[now] = 1;
+        result+=cost;
+        if(cost > check){
+            check = cost;
+        }
+        for(int i = 0;i < home[now].size();i++){
+            if(visited[home[now][i].first] == 0){
+                pq.push({home[now][i].second,home[now][i].first});
             }
         }
     }
