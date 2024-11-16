83ac8014597a765349e56c3c86d53a8578338bc5
wasdfg <leejh9711@naver.com>
2024-11-16 16:23:40 +0900

    [Silver I] Title: 1, 2, 3 더하기 5, Time: 8 ms, Memory: 7532 KB -BaekjoonHub

--- 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.cc
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.cc
@@ -4,24 +4,23 @@
 using namespace std;
 
 int main(void){
-    cin.tie(0);
     ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     int n,t;
     cin>>t;
-    vector<vector<long long>> dp(100001,vector<long long>(4,0));
-    dp[1][1] = 1;
-    dp[2][2] = 1;
-    dp[3][1] = 1;
-    dp[3][2] = 1;
-    dp[3][3] = 1;
+    vector<vector<long long>> dp(100001,vector<long long>(3,0));
+    dp[1][0] = 1;
+    dp[2][1] = 1;
+    dp[3][0] = dp[3][1] = dp[3][2] = 1;
     for(int i = 4;i < 100001;i++){
-            dp[i][1] = (dp[i-1][2]+dp[i-1][3]) % 1000000009;
-            dp[i][2] = (dp[i-2][1]+dp[i-2][3]) % 1000000009;
-            dp[i][3] = (dp[i-3][1]+dp[i-3][2]) % 1000000009;
+        dp[i][0] = (dp[i-1][1]+dp[i-1][2]) % 1000000009;
+        dp[i][1] = (dp[i-2][0]+dp[i-2][2]) % 1000000009 ;
+        dp[i][2] = (dp[i-3][0]+dp[i-3][1]) % 1000000009;
     }
-    for(int a = 0;a < t;a++){
+    for(int i = 0;i < t;i++){
         cin>>n;
-        cout<<(dp[n][1]+dp[n][2]+dp[n][3]) % 1000000009<<"\n";
+        cout<<(dp[n][0]+dp[n][1]+dp[n][2]) % 1000000009<<"\n";
     }
     return 0;
 }
