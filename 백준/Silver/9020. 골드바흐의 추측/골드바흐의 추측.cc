fba0cb4bdbba1e0c10dfe37f90ecc9f6879303da
wasdfg <leejh9711@naver.com>
2024-12-01 16:13:21 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 152 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.cc
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.cc
@@ -9,20 +9,20 @@ int main(void){
     vector<int> prime(10001,1);
     prime[0] = 0;
     prime[1] = 0;
-    for(int j = 2;j <= 10000;j++){
-        for(int k = 2;k*j <= 10000;k++){
-            prime[j*k] = 0;
+    for(int i = 2;i < 10001;i++){
+        for(int j = 2;j * i < 10001;j++){
+            prime[i*j] = 0;
         }
     }
     for(int i = 0;i < t;i++){
         cin>>n;
-        for(int j = n;j >= n/2;j--){
-            if((prime[j] * prime[n-j]) == 1){
-                high = j;
-                low = n-j;
+        for(int i = 2;i < (n+1)/2+1;i++){
+            if(prime[i]*prime[n-i] == 1){
+                high = n-i;
+                low = i;
             }
         }
-        cout<<low<<" "<<high<<endl;
+        cout<<low<<" "<<high<<"\n";
     }
     return 0;
 }
