d673d3106b381e90a33dfa00e4801f2df7aaf92b
wasdfg <leejh9711@naver.com>
2024-12-09 15:15:51 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 84 ms, Memory: 12488 KB -BaekjoonHub

--- 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -6,35 +6,39 @@ using namespace std;
 
 vector<int> graph[100001];
 vector<bool> visited(100001,0);
-vector<int> before(100001,0);
+vector<int> order;
 int cnt = 1;
 
-void dfs(int x,int check){
-    visited[x] = 1;
-    check = cnt;
-    before[x] = check;
-    for(int i = 0;i < graph[x].size();i++){
-        if(visited[graph[x][i]] == 0){
+void dfs(int now){
+    order[now] = cnt;
+    for(int i = 0;i < graph[now].size();i++){
+        if(visited[graph[now][i]] == 0){
+            visited[graph[now][i]] = 1;
             cnt++;
-            dfs(graph[x][i],cnt);
+            dfs(graph[now][i]);
         }
     }
 }
 
 int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int n,m,r,a,b;
     cin>>n>>m>>r;
+    order.assign(n+1,0);
     for(int i = 0;i < m;i++){
         cin>>a>>b;
         graph[a].push_back(b);
         graph[b].push_back(a);
     }
-    for(int i = 1;i <= n;i++){
+    for(int i = 1;i < n+1;i++){
         sort(graph[i].begin(),graph[i].end(),greater<int>());
     }
-    dfs(r,cnt);
-    for(int i = 1;i <= n;i++){
-        cout<<before[i]<<"\n";
+    visited[r] = 1;
+    dfs(r);
+    for(int i = 1;i < n+1;i++){
+        cout<<order[i]<<"\n";
     }
     return 0;
 }
