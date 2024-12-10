1ba5ee287641a107eba71a235cf90241b7764d1e
wasdfg <leejh9711@naver.com>
2024-12-10 14:01:24 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 88 ms, Memory: 9444 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -5,41 +5,41 @@
 
 using namespace std;
 
-vector<int> graph[100001];
-
 int main(void){
     ios::sync_with_stdio(false);
-    cin.tie(0);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
     int n,m,r,a,b,cnt = 1;
-    vector<int> before(100001,0);
-    vector<bool> visited(100001,0);
-    queue<int> togo;
     cin>>n>>m>>r;
+    vector<int> graph[n+1];
+    vector<bool> visited(n+1,0);
+    vector<int> order(n+1,0);
     for(int i = 0;i < m;i++){
         cin>>a>>b;
         graph[a].push_back(b);
         graph[b].push_back(a);
     }
-    for(int i = 1;i <= n;i++){
+    for(int i = 1;i < n+1;i++){
         sort(graph[i].begin(),graph[i].end());
     }
+    order[r] = 1;
     visited[r] = 1;
-    before[r] = cnt;
+    queue<int> togo;
     togo.push(r);
     while(!togo.empty()){
-        int x = togo.front();
+        int now = togo.front();
         togo.pop();
-        for(int i = 0;i < graph[x].size();i++){
-            if(visited[graph[x][i]] == 0){
-                visited[graph[x][i]] = 1;
+        for(int i = 0;i < graph[now].size();i++){
+            if(visited[graph[now][i]] == 0){
+                visited[graph[now][i]] = 1;
                 cnt++;
-                before[graph[x][i]] = cnt;
-                togo.push({graph[x][i]});
+                order[graph[now][i]] = cnt;
+                togo.push(graph[now][i]);
             }
         }
     }
-    for(int i = 1;i <= n;i++){
-        cout<<before[i]<<"\n";
+    for(int i = 1;i < n+1;i++){
+        cout<<order[i]<<"\n";
     }
     return 0;
 }
