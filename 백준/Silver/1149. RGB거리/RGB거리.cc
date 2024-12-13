4cd0004c71b1cb22991ce411005cb7694850ae09
wasdfg <leejh9711@naver.com>
2024-12-13 14:35:49 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -5,21 +5,15 @@
 using namespace std;
 
 int main(void){
-    int n;
+    int n,a,b,c;
     cin>>n;
-    vector<vector<int>> rgb(n,vector<int>(3,0));
-    vector<vector<int>> dp(n,vector<int>(3,0));
-    for(int i = 0;i < n;i++){
-        cin>>rgb[i][0]>>rgb[i][1]>>rgb[i][2];
+    vector<vector<int>> dp(n+1,vector<int>(3,0));
+    for(int i = 1;i < n+1;i++){
+        cin>>a>>b>>c;
+        dp[i][0] = min(dp[i-1][1],dp[i-1][2])+a;
+        dp[i][1] = min(dp[i-1][0],dp[i-1][2])+b;
+        dp[i][2] = min(dp[i-1][0],dp[i-1][1])+c;
     }
-    for(int i = 0;i < 3;i++){
-        dp[0][i] = rgb[0][i];
-    }
-    for(int i = 1;i < n;i++){
-        dp[i][0] = rgb[i][0]+min(dp[i-1][1],dp[i-1][2]);
-        dp[i][1] = rgb[i][1]+min(dp[i-1][0],dp[i-1][2]);
-        dp[i][2] = rgb[i][2]+min(dp[i-1][0],dp[i-1][1]);
-    }
-    cout<<min({dp[n-1][0],dp[n-1][1],dp[n-1][2]});
+    cout<<min({dp[n][0],dp[n][1],dp[n][2]});
     return 0;
 }
