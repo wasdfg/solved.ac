44b5d62710611ff15559ebf0c6b52d8a95b507d5
wasdfg <leejh9711@naver.com>
2024-12-14 15:14:11 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -5,55 +5,51 @@
 
 using namespace std;
 
-vector<int> vertex[1001];
-vector<bool> visited(1001,0);
-vector<int> dfs;
-vector<int> bfs;
+vector<int> graph[1001];
+vector<bool> visited;
 
-int BFS(int start){
+void dfs(int now){
+    for(int i = 0;i < graph[now].size();i++){
+        if(visited[graph[now][i]] == 0){
+            visited[graph[now][i]] = 1;
+            cout<<graph[now][i]<<" ";
+            dfs(graph[now][i]); 
+        }
+    }
+}
+
+
+int main(void){
+    int n,m,v,a,b;
+    cin>>n>>m>>v;
+    visited.assign(n+1,0);
+    for(int i = 0;i < m;i++){
+        cin>>a>>b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+    for(int i = 1;i < n+1;i++){
+        sort(graph[i].begin(),graph[i].end());
+    }
+    visited[v] = 1;
+    cout<<v<<" ";
+    dfs(v);
+    cout<<"\n";
+    visited.assign(n+1,0);
     queue<int> togo;
-    togo.push(start);
-    visited[start] = 1;
+    togo.push(v);
+    visited[v] = 1;
+    cout<<v<<" ";
     while(!togo.empty()){
-        int x = togo.front();
-        cout<<x<<" ";
+        int now = togo.front();
         togo.pop();
-        for(int i = 0;i < vertex[x].size();i++){
-            if(!visited[vertex[x][i]]){
-                togo.push(vertex[x][i]);
-                visited[vertex[x][i]] = 1;
+        for(int i = 0;i < graph[now].size();i++){
+            if(visited[graph[now][i]] == 0){
+                visited[graph[now][i]] = 1;
+                cout<<graph[now][i]<<" ";
+                togo.push(graph[now][i]);
             }
         }
     }
-    return 0;
-}
-
-int DFS(int start){
-    visited[start] = 1;
-    cout<<start<<" ";
-    for(int i = 0;i < vertex[start].size();i++){
-        if(!visited[vertex[start][i]]){
-            DFS(vertex[start][i]);
-        }
-    }
-    return 0;
-}
-
-int main(void){
-    int dot,line,start,a,b;
-    cin>>dot>>line>>start;
-    for(int i = 0;i < line;i++){
-        cin>>a>>b;
-        vertex[a].push_back(b);
-        vertex[b].push_back(a);
-    }
-    for(int i = 1;i < 1001;i++){
-        sort(vertex[i].begin(),vertex[i].end());
-    }
-    DFS(start);
-    cout<<"\n";
-    visited.assign(1001,0);
-    BFS(start);
-    cout<<"\n";
     return 0;
 }
