6d658e8919837b7735eb4d7f69c02594c38ca0bc
wasdfg <leejh9711@naver.com>
2024-12-15 14:00:32 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,52 +1,48 @@
 #include<iostream>
+#include<algorithm>
 #include<vector>
 #include<queue>
-#include<algorithm>
 
 using namespace std;
 
 int main(void){
-    int n,count = 0;
-    cin>>n;
-    vector<int> result;
-    vector<vector<bool>> area(n,vector<bool>(n,0));
-    vector<vector<bool>> visited(n,vector<bool>(n,0));
     int dx[4] = {0,0,-1,1};
     int dy[4] = {1,-1,0,0};
-    queue<pair<int,int>> togo;
+    int n,area;
+    cin>>n;
+    vector<vector<int>> graph(n,vector<int>(n,0));
+    vector<int> areas;
     for(int i = 0;i < n;i++){
         for(int j = 0;j < n;j++){
-            int t;
-            scanf("%1d",&t);
-            area[i][j] = t;
+            scanf("%1d",&graph[i][j]);
         }
     }
     for(int i = 0;i < n;i++){
         for(int j = 0;j < n;j++){
-            if(area[i][j] == 1 && visited[i][j] == 0){
-                visited[i][j] = 1;
+            if(graph[i][j] == 1){
+                graph[i][j] = 0;
+                queue<pair<int,int>> togo;
                 togo.push({i,j});
-                count = 1;
+                area = 1;
                 while(!togo.empty()){
                     int x = togo.front().first;
                     int y = togo.front().second;
                     togo.pop();
-                    for(int k = 0;k < 4;k++){
-                        if(x+dx[k] >= 0 && x+dx[k] < n && y+dy[k] >= 0 && y+dy[k] < n && visited[x+dx[k]][y+dy[k]] == 0 && area[x+dx[k]][y+dy[k]] == 1){
-                            visited[x+dx[k]][y+dy[k]] = 1;
-                            count++;
-                            togo.push({x+dx[k],y+dy[k]});
+                    for(int a = 0;a < 4;a++){
+                        if(x+dx[a] >= 0 && x+dx[a] < n && y+dy[a] >= 0 && y+dy[a] < n && graph[x+dx[a]][y+dy[a]] == 1){
+                            graph[x+dx[a]][y+dy[a]] = 0;
+                            area++;
+                            togo.push({x+dx[a],y+dy[a]});
                         }
                     }
                 }
-                result.push_back(count);
+                areas.push_back(area);
             }
         }
     }
-    sort(result.begin(),result.end());
-    cout<<result.size()<<endl;
-    for(auto a:result){
-        cout<<a<<endl;
+    sort(areas.begin(),areas.end());
+    cout<<areas.size()<<"\n";
+    for(int i = 0;i < areas.size();i++){
+        cout<<areas[i]<<"\n";
     }
-    return 0;
 }
