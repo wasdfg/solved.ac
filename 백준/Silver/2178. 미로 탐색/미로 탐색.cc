a5ae0db7c152880522a0aabde10972f65c9cc007
wasdfg <leejh9711@naver.com>
2024-12-18 14:14:01 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -10,31 +10,29 @@ int main(void){
     int dx[4] = {0,0,-1,1};
     int dy[4] = {1,-1,0,0};
     vector<vector<int>> maze(n,vector<int>(m,0));
-    vector<vector<int>> dp(n,vector<int>(m,0));
-    queue<pair<int,int>> togo;
+    queue<pair<pair<int,int>,int>> togo;
     for(int i = 0;i < n;i++){
         for(int j = 0;j < m;j++){
             scanf("%1d",&maze[i][j]);
         }
     }
-    dp[0][0] = 1;
     maze[0][0] = 0;
-    togo.push({0,0});
+    togo.push({{0,0},1});
     while(!togo.empty()){
-        int x = togo.front().first;
-        int y = togo.front().second;
+        int x = togo.front().first.first;
+        int y = togo.front().first.second;
+        int cost = togo.front().second;
         togo.pop();
         if(x == n-1 && y == m-1){
+            cout<<cost;
             break;
         }
         for(int i = 0;i < 4;i++){
             if(x+dx[i] >= 0 && x+dx[i] < n && y+dy[i] >= 0 && y+dy[i] < m && maze[x+dx[i]][y+dy[i]] == 1){
-                dp[x+dx[i]][y+dy[i]] = dp[x][y]+1;
                 maze[x+dx[i]][y+dy[i]] = 0;
-                togo.push({x+dx[i],y+dy[i]});
+                togo.push({{x+dx[i],y+dy[i]},cost+1});
             }
         }
     }
-    cout<<dp[n-1][m-1];
     return 0;
 }
