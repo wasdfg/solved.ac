e5e0535cd9b36c04492b32abb1dbe5c96ccabae7
wasdfg <leejh9711@naver.com>
2024-12-19 14:26:46 +0900

    [Gold V] Title: AC, Time: 48 ms, Memory: 3116 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.cc
+++ 백준/Gold/5430. AC/AC.cc
@@ -6,63 +6,62 @@
 using namespace std;
 
 int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
     int t,n;
     string p,x;
     cin>>t;
     for(int i = 0;i < t;i++){
-        deque<int> num;
-        string chgnum = "";
-        bool error = 0,back = 0;
         cin>>p>>n>>x;
-        for(int j = 1;j < x.length();j++){
-            if(x[1] == ']'){
-                break;
+        deque<int> num;
+        bool checkerror = false;
+        bool foward = true;
+        string tonum = "";
+        for(int len = 0;len < x.length();len++){
+            if(x[len] >= '0' && x[len] <= '9'){
+                tonum+=x[len];
             }
-            if(x[j] >= 48 && x[j] < 58){
-                chgnum+=(x[j]);
-            }
-            else{
-                num.push_back(stoi(chgnum));
-                chgnum = "";
+            if(x[len] == ',' || x[len] == ']'){
+                if(tonum.size() != 0){
+                    num.push_back(stoi(tonum));
+                }
+                tonum = "";
             }
         }
-        for(int j = 0;j < p.length();j++){
-            if(p[j] == 'R'){
-                back = !back;
+        for(int len = 0;len < p.length();len++){
+            if(p[len] == 'R'){
+                foward = !foward;
             }
-            else{
+            if(p[len] == 'D'){
                 if(num.empty()){
-                    error = 1;
+                    checkerror = true;
+                    break;
+                }
+                if(foward == true){
+                    num.pop_front();
                 }
                 else{
-                    if(back == 0){
-                        num.pop_front();
-                    }
-                    else{
-                        num.pop_back();
-                    }
+                    num.pop_back();
                 }
             }
         }
-        if(error == 1){
+        if(checkerror == true){
             cout<<"error\n";
         }
         else{
             cout<<"[";
-            if(back == 1){
-                for(int j = num.size()-1;j >= 0;j--){
-                    cout<<num[j];
-                    if(j > 0){
-                        cout<<",";
-                    }
+            while(!num.empty()){
+                if(foward == true){
+                    cout<<num.front();
+                    num.pop_front();
                 }
-            }
-            else{
-                for(int j = 0;j < num.size();j++){
-                    cout<<num[j];
-                    if(j < num.size() - 1){
-                        cout<<",";
-                    }
+                else{
+                    cout<<num.back();
+                    num.pop_back();
+                }
+                if(num.size() > 0){
+                    cout<<",";
                 }
             }
             cout<<"]\n";
