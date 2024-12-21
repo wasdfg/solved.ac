dd5b8feb08fce8688702a8a6568e3b8d43d1cea2
wasdfg <leejh9711@naver.com>
2024-12-21 16:05:05 +0900

    [Gold V] Title: 토마토, Time: 220 ms, Memory: 7256 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.cc
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -4,15 +4,15 @@
 #include<tuple>
 
 using namespace std;
-int tomato[100][100][100];
 
 int main(void){
     int n,m,h,result = 0;
-    int dx[6] = {1,-1,0,0,0,0};
-    int dy[6] = {0,0,-1,1,0,0};
-    int dz[6] = {0,0,0,0,-1,1};
-    queue<tuple<int,int,int>> togo;
+    int dx[6] = {0,0,-1,1,0,0};
+    int dy[6] = {1,-1,0,0,0,0};
+    int dz[6] = {0,0,0,0,1,-1}; 
     cin>>m>>n>>h;
+    int tomato[h][n][m];
+    queue<tuple<int,int,int>> togo;
     for(int i = 0;i < h;i++){
         for(int j = 0;j < n;j++){
             for(int k = 0;k < m;k++){
@@ -24,36 +24,30 @@ int main(void){
         }
     }
     while(!togo.empty()){
-        int x = get<0>(togo.front());
-        int y = get<1>(togo.front());
-        int z = get<2>(togo.front());
+        int x = get<1>(togo.front());
+        int y = get<2>(togo.front());
+        int z = get<0>(togo.front());
         togo.pop();
         for(int i = 0;i < 6;i++){
-            if(x+dx[i] >= 0 && x+dx[i] < h && y+dy[i] >= 0 && y+dy[i] < n && z+dz[i] >= 0 && z+dz[i] < m){
-                if(tomato[x+dx[i]][y+dy[i]][z+dz[i]] == 0){
-                    tomato[x+dx[i]][y+dy[i]][z+dz[i]] = tomato[x][y][z]+1;
-                    togo.push({x+dx[i],y+dy[i],z+dz[i]});
-                }
-            } 
+            if(x+dx[i] >= 0 && x+dx[i] < n && y+dy[i] >= 0 && y+dy[i] < m && z+dz[i] >= 0 && z+dz[i] < h && tomato[z+dz[i]][x+dx[i]][y+dy[i]] == 0){
+                tomato[z+dz[i]][x+dx[i]][y+dy[i]] = tomato[z][x][y]+1;
+                togo.push({z+dz[i],x+dx[i],y+dy[i]});
+            }
         }
     }
-    bool cant = false;
     for(int i = 0;i < h;i++){
         for(int j = 0;j < n;j++){
             for(int k = 0;k < m;k++){
                 if(tomato[i][j][k] == 0){
-                    cant = true;
-                    break;
+                    cout<<"-1";
+                    return 0;
                 }
-                result = max(result,tomato[i][j][k]);
+                if(result < tomato[i][j][k]){
+                    result = tomato[i][j][k];
+                }
             }
         }
     }
-    if(cant == 1){
-        cout<<"-1";
-    }
-    else{
-        cout<<result-1;
-    }
+    cout<<result-1;
     return 0;
 }
