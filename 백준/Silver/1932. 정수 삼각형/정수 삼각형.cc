9937029267b59d50d44fc927f0438cc7cbf35f9f
wasdfg <leejh9711@naver.com>
2025-01-11 13:42:19 +0900

    [Silver I] Title: 정수 삼각형, Time: 36 ms, Memory: 2944 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,11 +1,10 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
 
 using namespace std;
 
 int main(void){
-    int n,x;
+    int n,x = 0;
     cin>>n;
     vector<vector<int>> tri(n,vector<int>(n+1,0));
     for(int i = 0;i < n;i++){
@@ -13,16 +12,19 @@ int main(void){
             cin>>tri[i][j];
         }
     }
-    for(int i = 1;i < tri.size();i++){
+    for(int i = 1;i < n;i++){
         for(int j = 0;j < i+1;j++){
             if(j == 0){
-                tri[i][j]+=tri[i-1][j];
+                tri[i][0]+=tri[i-1][0];
             }
             else{
-                tri[i][j]+=max({tri[i-1][j],tri[i-1][j-1]});
+                tri[i][j]+=max(tri[i-1][j],tri[i-1][j-1]);
             }
         }
     }
-    cout<<*max_element(tri[n-1].begin(),tri[n-1].end());
+    for(int i = 0;i < n+1;i++){
+        x = max(x,tri[n-1][i]);
+    }
+    cout<<x;
     return 0;
 }
