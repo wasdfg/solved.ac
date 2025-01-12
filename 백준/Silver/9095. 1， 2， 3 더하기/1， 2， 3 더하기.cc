514403f02846cc1ea9f3db87dfe58fc9de645a9c
wasdfg <leejh9711@naver.com>
2025-01-12 13:14:11 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -6,16 +6,16 @@ using namespace std;
 int main(void){
     int t,n;
     cin>>t;
-    vector<int> ott(12,0);
+    vector<int> ott(11,0);
     ott[1] = 1;
     ott[2] = 2;
     ott[3] = 4;
-    for(int i = 4;i <= 11;i++){
+    for(int i = 4;i < 11;i++){
         ott[i] = ott[i-1]+ott[i-2]+ott[i-3];
     }
     for(int i = 0;i < t;i++){
         cin>>n;
-        cout<<ott[n]<<endl;
+        cout<<ott[n]<<"\n";
     }
     return 0;
 }
