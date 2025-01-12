62d9c3d2bb5362794f8508408e8ec7ee2f5355e9
wasdfg <leejh9711@naver.com>
2025-01-12 13:46:28 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2416 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -7,12 +7,11 @@ using namespace std;
 int main(void){
     int n,k;
     cin>>n>>k;
-    vector<int> pos(100001,100000000);
+    vector<int> pos(100001,0);
     vector<bool> visited(100001,0);
     queue<int> now;
     now.push(n);
     visited[n] = 1;
-    pos[n] = 0;
     while(!now.empty()){
         int here = now.front();
         now.pop();
@@ -20,17 +19,17 @@ int main(void){
             break;
         }
         if(here+1 < 100001 && visited[here+1] == 0){
-            pos[here+1] = min(pos[here+1],pos[here]+1);
+            pos[here+1] = pos[here]+1;
             visited[here+1] = 1;
             now.push(here+1);
         }
         if(here-1 >= 0 && visited[here-1] == 0){
-            pos[here-1] = min(pos[here-1],pos[here]+1);
+            pos[here-1] = pos[here]+1;
             visited[here-1] = 1;
             now.push(here-1);
         }
         if(here*2 < 100001 && visited[here*2] == 0){
-            pos[here*2] = min(pos[here*2],pos[here]+1);
+            pos[here*2] = pos[here]+1;
             visited[here*2] = 1;
             now.push(here*2);
         }
